f738df40682e752795c215babfe2e780c917b2fa
Conrad Akunga <conradakunga@gmail.com>
2020-05-28 19:33:05 +0300

    Concatenation part 1 code

---
+++ concatenation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace concatenation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Homer";
+            var surname = "Simpson";
+            var fullName = firstName + " " + surname;
+            Console.WriteLine(fullName);
+
+
+            var title = "His Excellency";
+            var initial = "J";
+            fullName = title + " " + firstName + " " + initial + " " + surname;
+            Console.WriteLine(fullName);
+
+            var children = "Bart" + " " + "Lisa" + "Maggie";
+            Console.WriteLine(children);
+        }
+    }
+}
