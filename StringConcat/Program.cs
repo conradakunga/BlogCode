e1f96d1ac572c3951804312419eee22ad2e674dc
Conrad Akunga <conradakunga@gmail.com>
2020-06-15 14:42:57 +0300

    String.concat

---
+++ StringConcat/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StringConcat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var son = String.Concat("Bart", "Simpson");
+
+            Console.WriteLine(son);
+
+            var daughter = String.Concat("Lisa", "Marie", "Simpson");
+
+            Console.WriteLine(daughter);
+
+            var mother = String.Concat("Marjorie", "Jacqueline", "Bouvier", "Simpson");
+
+            Console.WriteLine(mother);
+
+            var clown = String.Concat("Herschel", "Shmoikel", "Pinchas", "Yerucham", "Krustofsky");
+
+            Console.WriteLine(clown);
+        }
+    }
+}
