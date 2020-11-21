bebfee3efd4f6cbf16e726845f2259b5d164e463
Conrad Akunga <conradakunga@gmail.com>
2020-11-21 11:53:20 +0300

    Version 2; type of age change

--- RecordExample/RecordExample/Person.cs
+++ RecordExample/RecordExample/Person.cs
@@ -7,7 +7,7 @@ namespace RecordExample
         public string FirstName { get; set; }
         public string Surname { get; set; }
         public string FullNames { get; set; }
-        public int Age { get; set; }
+        public byte Age { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string NickName { get; set; }
     }
