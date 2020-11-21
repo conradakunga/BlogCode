eec3bf83b23b41ed2f9182fadfc4e0039c228088
Conrad Akunga <conradakunga@gmail.com>
2020-11-21 12:22:28 +0300

    Added unit test

---
+++ RecordExample/RecordExample.Tests/PersonTest.cs
@@ -0,0 +1,27 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace RecordExample.Tests
+{
+    public class PersonTests
+    {
+        [Fact]
+        public void Person_Is_Constructed_Correctly()
+        {
+            var p = new Person()
+            {
+                FirstName = "Bart",
+                Surname = "Simpson",
+                DateOfBirth = new DateTime(2000, 1, 1),
+                NickName = "JoJo"
+            };
+            p.FirstName.Should().Be("Bart");
+            p.Surname.Should().Be("Simpson");
+            p.DateOfBirth.Should().Be(new DateTime(2000, 1, 1));
+            p.NickName.Should().Be("JoJo");
+            p.FullNames.Should().Be("Simpson, Bart");
+            p.Age.Should().Be(20);
+        }
+    }
+}
