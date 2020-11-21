2945c58c3d9926d4e773174cf5dc013d4220992e
Conrad Akunga <conradakunga@gmail.com>
2020-11-21 12:03:27 +0300

    Version 3: Age as an expression

--- RecordExample/RecordExample/Person.cs
+++ RecordExample/RecordExample/Person.cs
@@ -7,7 +7,7 @@ namespace RecordExample
         public string FirstName { get; set; }
         public string Surname { get; set; }
         public string FullNames { get; set; }
-        public byte Age { get; set; }
+        public byte Age => (byte)(DateTime.Now.Year - DateOfBirth.Year);
         public DateTime DateOfBirth { get; set; }
         public string NickName { get; set; }
     }
