fe5bb308f430945ae618696ac13d4c1d0819363e
Conrad Akunga <conradakunga@gmail.com>
2020-11-21 12:36:04 +0300

    Version 5; Make immutable

--- RecordExample/RecordExample/Person.cs
+++ RecordExample/RecordExample/Person.cs
@@ -4,11 +4,14 @@ namespace RecordExample
 {
     public class Person
     {
-        public string FirstName { get; set; }
-        public string Surname { get; set; }
+        public string FirstName { get; }
+        public string Surname { get; }
         public string FullNames => $"{Surname}, {FirstName}";
         public byte Age => (byte)(DateTime.Now.Year - DateOfBirth.Year);
-        public DateTime DateOfBirth { get; set; }
-        public string NickName { get; set; }
+        public DateTime DateOfBirth { get; }
+        public string NickName { get; }
+
+        public Person(string firstName, string surname, DateTime dateOfBirth, string nickName) =>
+            (FirstName, Surname, DateOfBirth, NickName) = (firstName, surname, dateOfBirth, nickName);
     }
 }
