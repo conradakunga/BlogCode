fcf5ea2c432fa60fec121cb6c3e06eec7d8a63b7
Conrad Akunga <conradakunga@gmail.com>
2020-11-21 13:17:38 +0300

    Cloning objects based on others

--- RecordExample/RecordExample.Tests/PersonTest.cs
+++ RecordExample/RecordExample.Tests/PersonTest.cs
@@ -24,5 +24,27 @@ namespace RecordExample.Tests
             p.FullNames.Should().Be("Simpson, Bart");
             p.Age.Should().Be(20);
         }
+        [Fact]
+        public void Person_Copy_Objects_Behave_Correctly()
+        {
+            var bart = new Person()
+            {
+                FirstName = "Bart",
+                Surname = "Simpson",
+                DateOfBirth = new DateTime(2000, 1, 1),
+                NickName = "JoJo"
+            };
+
+            var bartsTwinSister = bart with { FirstName = "Lisa" };
+
+            bartsTwinSister.FirstName.Should().Be("Lisa");
+            bartsTwinSister.Surname.Should().Be("Simpson");
+            bartsTwinSister.DateOfBirth.Should().Be(new DateTime(2000, 1, 1));
+            bartsTwinSister.NickName.Should().Be("JoJo");
+            bartsTwinSister.FullNames.Should().Be("Simpson, Lisa");
+            bartsTwinSister.Age.Should().Be(20);
+
+
+        }
     }
 }
