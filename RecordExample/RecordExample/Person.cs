c3bda2ca8685b12434f05e4b0c32aa00625321c5
Conrad Akunga <conradakunga@gmail.com>
2020-11-21 12:09:14 +0300

    Version 4: fullnames as expression

--- RecordExample/RecordExample/Person.cs
+++ RecordExample/RecordExample/Person.cs
@@ -6,7 +6,7 @@ namespace RecordExample
     {
         public string FirstName { get; set; }
         public string Surname { get; set; }
-        public string FullNames { get; set; }
+        public string FullNames => $"{Surname}, {FirstName}";
         public byte Age => (byte)(DateTime.Now.Year - DateOfBirth.Year);
         public DateTime DateOfBirth { get; set; }
         public string NickName { get; set; }
