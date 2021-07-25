95f070d4774fff8fdd767984106b9f036f076f87
Conrad Akunga <conradakunga@gmail.com>
2021-07-25 13:03:20 +0300

    Completed tests

--- FluentTesting/NUnitTests/NUnitTests.cs
+++ FluentTesting/NUnitTests/NUnitTests.cs
@@ -1,3 +1,5 @@
+using System;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace NUnitTests
@@ -8,7 +10,37 @@ namespace NUnitTests
         public void Test_For_Sum()
         {
             var sum = 2 + 5;
-            Assert.AreEqual(sum, 7);
+            Assert.AreEqual(7, sum);
+        }
+        [Test]
+        public void Test_For_Sum2()
+        {
+            var sum = 2 + 5;
+            sum.Should().Be(7);
+        }
+        [Test]
+        public void Test_For_Sum3()
+        {
+            var sum = 2 + 5;
+            sum.Should().Be(7).And.BeGreaterThan(0);
+        }
+        [Test]
+        public void Test_For_Exception()
+        {
+            int denom = 0;
+            var result = 0;
+            Assert.Throws<DivideByZeroException>(() => result = 6 / denom);
+        }
+        [Test]
+        public void Test_For_Exception2()
+        {
+            int denom = 0;
+            var result = 0;
+            // Create an action
+            Action action = () => result = 6 / denom;
+            // Attemt to execute the action
+            action.Should().Throw<DivideByZeroException>()
+                .WithMessage("Attempted to divide by zero.");
         }
     }
 }
--- FluentTesting/XUnitTests/XUnitTests.cs
+++ FluentTesting/XUnitTests/XUnitTests.cs
@@ -1,4 +1,5 @@
 using System;
+using FluentAssertions;
 using Xunit;
 
 namespace XUnitTests
@@ -9,7 +10,38 @@ namespace XUnitTests
         public void Test_For_Sum()
         {
             var sum = 2 + 5;
-            Assert.Equal(sum, 7);
+            Assert.Equal(7, sum);
+        }
+        [Fact]
+        public void Test_For_Sum2()
+        {
+            var sum = 2 + 5;
+            sum.Should().Be(7);
+        }
+        [Fact]
+        public void Test_For_Sum3()
+        {
+            var sum = 2 + 5;
+            sum.Should().Be(7).And.BeGreaterThan(0);
+        }
+        [Fact]
+        public void Test_For_Exception()
+        {
+            int denom = 0;
+            var result = 0;
+            Assert.Throws<DivideByZeroException>(() => result = 6 / denom);
+        }
+        [Fact]
+        public void Test_For_Exception2()
+        {
+            int denom = 0;
+            var result = 0;
+            // Create an action
+            Action action = () => result = 6 / denom;
+            // Attemt to execute the action
+            action.Should().Throw<DivideByZeroException>()
+                .WithMessage("Attempted to divide by zero.");
+
         }
     }
 }
