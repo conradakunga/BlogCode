6896c5ca22a01ae5dbf7d0414da5533f24b3697e
Conrad Akunga <conradakunga@gmail.com>
2021-09-10 18:28:48 +0300

    Math API additions

---
+++ 2021-09-10 - 30 Days Of .NET 6 - Day 5 - Math API Additions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace SineCosine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Test>();
+        }
+    }
+    public class Test
+    {
+        const double angle = 35;
+        [Benchmark]
+        public (double, double) GetIndividually()
+        {
+            var sin = Math.Sin(angle);
+            var cos = Math.Cos(angle);
+            return (sin, cos);
+        }
+         [Benchmark]
+        public (double, double) GetSimultaneously()
+        {
+            var result = Math.SinCos(angle);
+            return (result.Sin, result.Cos);
+        }
+    }
+}
