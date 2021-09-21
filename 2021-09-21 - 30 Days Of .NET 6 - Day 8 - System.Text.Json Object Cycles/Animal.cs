da11d36b3dc31716719df7e59685228e6acd3462
Conrad Akunga <conradakunga@gmail.com>
2021-09-21 09:48:32 +0300

    Added System.Text.JSON object cycle handling

---
+++ 2021-09-21 - 30 Days Of .NET 6 - Day 8 - System.Text.Json Object Cycles/Animal.cs
@@ -0,0 +1,6 @@
+public class Animal
+{
+    public string Name { get; set; }
+    public byte Legs { get; set; }
+    public Animal Parent { get; set; }
+}
---
+++ 2021-09-21 - 30 Days Of .NET 6 - Day 8 - System.Text.Json Object Cycles/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var cat = new Animal() { Name = "Cat", Legs = 4 };
+cat.Parent = cat;
+
+Console.WriteLine($"This is a {cat.Name} and it's parent is {cat.Parent.Name}");
+
+var options = new JsonSerializerOptions() { ReferenceHandler = ReferenceHandler.Preserve };
+var serializedString = JsonSerializer.Serialize(cat, options);
+
+Console.WriteLine(serializedString);
+
+options = new JsonSerializerOptions() { ReferenceHandler = ReferenceHandler.IgnoreCycles };
+serializedString = JsonSerializer.Serialize(cat, options);
+
+Console.WriteLine(serializedString);
+
+
+
