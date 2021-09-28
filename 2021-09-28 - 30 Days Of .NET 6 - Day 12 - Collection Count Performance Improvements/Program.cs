d9aa280437f2747c8d403469a76cd32a380bd340
Conrad Akunga <conradakunga@gmail.com>
2021-09-28 11:09:30 +0300

    Add collection count performance improvements

---
+++ 2021-09-28 - 30 Days Of .NET 6 - Day 12 - Collection Count Performance Improvements/Program.cs
@@ -0,0 +1,68 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Configs;
+
+BenchmarkRunner.Run<Test>();
+ 
+[GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+[CategoriesColumn]
+public class Test
+{
+    IEnumerable<int> enumerable;
+    List<int> list;
+    int[] array;
+
+    public Test()
+    {
+        enumerable = Enumerable.Range(0, 25);
+        list = enumerable.ToList();
+        array = enumerable.ToArray();
+    }
+
+    [Benchmark(Baseline = true)]
+    [BenchmarkCategory("Enumerable")]
+    public int GetEnumerableCountOld()
+    {
+        var count = enumerable.Count();
+        return count;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Enumerable")]
+    public int GetEnumerableCountNew()
+    {
+        enumerable.TryGetNonEnumeratedCount(out var count);
+        return count;
+    }
+
+    [Benchmark(Baseline = true)]
+    [BenchmarkCategory("List")]
+    public int GetListCountOld()
+    {
+        var count = list.Count();
+        return count;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("List")]
+    public int GetListCountNew()
+    {
+        list.TryGetNonEnumeratedCount(out var count);
+        return count;
+    }
+
+    [Benchmark(Baseline = true)]
+    [BenchmarkCategory("Array")]
+    public int GetArrayCountOld()
+    {
+        var count = array.Count();
+        return count;
+    }
+    [Benchmark]
+    [BenchmarkCategory("Array")]
+    public int GetArrayCountNew()
+    {
+        array.TryGetNonEnumeratedCount(out var count);
+        return count;
+    }
+}
