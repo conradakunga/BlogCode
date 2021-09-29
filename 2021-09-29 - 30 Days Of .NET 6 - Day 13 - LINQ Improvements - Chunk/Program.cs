0dc6c25a68e245b8dcd6c1ad3357450820cf9e84
Conrad Akunga <conradakunga@gmail.com>
2021-09-29 13:10:31 +0300

    LINQ Chunking

---
+++ 2021-09-29 - 30 Days Of .NET 6 - Day 13 - LINQ Improvements - Chunk/Program.cs
@@ -0,0 +1,15 @@
+﻿var range = Enumerable.Range(0, 50).ToArray();
+
+// Partition the range into chunks of 10
+var chunks = range.Chunk(size: 10).ToList();
+
+// Loop though each chunk and process
+for (var i = 0; i < chunks.Count(); i++)
+{
+    Console.WriteLine($"Processing chunk {i}");
+    Print(chunks[i]);
+    // Call our expensive service and pass the partition
+    // CallExpensiveService(chunks[i]);
+}
+
+void Print(int[] input) => Console.WriteLine(string.Join(",", input));
