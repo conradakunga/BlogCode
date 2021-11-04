1c6f55f31d863b3feadf8a3e58d66df1f1270da4
Conrad Akunga <conradakunga@gmail.com>
2021-11-04 13:05:41 +0300

    Init commit

---
+++ NullableRef/Program.cs
@@ -0,0 +1,11 @@
+﻿var names = new string[] { "Bartholomew", "Matthew", "Simon", "Jude" };
+
+// Here the compiler will complain about a possible null
+string notNullName = names.FirstOrDefault(n => n.Length > 20);
+
+// This will have no problem
+string? possibleNullName = names.FirstOrDefault(n => n.Length > 20);
+
+Console.WriteLine(notNullName);
+
+Console.WriteLine(possibleNullName);
