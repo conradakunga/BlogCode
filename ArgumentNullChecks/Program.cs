7675c3299408922d361a0301fd6e595a0b678bd8
Conrad Akunga <conradakunga@gmail.com>
2021-12-21 12:45:37 +0300

    Argument null checks

---
+++ ArgumentNullChecks/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine(Concatenate(null, "two"));
+
+string Concatenate(string first, string second)
+{
+    ArgumentNullException.ThrowIfNull(first);
+    ArgumentNullException.ThrowIfNull(second);
+
+    return first + second;
+}
