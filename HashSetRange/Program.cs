1efc45a8c6f122297c43faaaf61a3830eceaa111
Conrad Akunga <conradakunga@gmail.com>
2022-04-29 00:14:23 +0300

    Init commit

---
+++ HashSetRange/Program.cs
@@ -0,0 +1,24 @@
+﻿// a list of persons
+var personList = new List<Person>();
+
+// add multiple people
+
+personList.AddRange(new[] { new Person("James", "Bond"), new Person("Jason", "Bourne") });
+
+// a hashset of persons
+
+var personHashSet = new HashSet<Person>();
+
+// add multiple people
+
+personHashSet.UnionWith(new[] { new Person("Evelyn", "Salt"), new Person("Napoleon", "Solo") });
+
+// add people from existing collection
+
+personHashSet.UnionWith(personList);
+
+foreach (var person in personHashSet)
+    Console.WriteLine($"{person.FirstName} {person.Surname}");
+
+
+public record Person(string FirstName, string Surname);
