b81a7a493b87c99d9f569060369d44b52559dcc1
Conrad Akunga <conradakunga@gmail.com>
2022-08-15 13:55:13 +0300

    Url building

---
+++ UriConstruction/Program.cs
@@ -0,0 +1,49 @@
+﻿using Flurl;
+
+var client = new HttpClient();
+
+var result = await client.GetStringAsync("https://localhost:5000");
+
+await client.GetStringAsync("https://localhost:" + 5000.ToString());
+
+await client.GetStringAsync($"https://localhost:{5000}");
+
+await client.GetStringAsync($"https://localhost:{5000}/Customers?Height=10&Weight=70");
+
+var query = new Dictionary<string, string>()
+{
+    {"Height","10"},
+    {"Weight" , "70"},
+    {"BirthYear", "2000"}
+};
+var queryString = string.Join('&', query.Select(q => $"{q.Key}={q.Value}"));
+
+var builder = new UriBuilder("https", "localhost", 5000, "Customers", $"?{queryString}");
+
+await client.GetStringAsync(builder.Uri);
+
+
+// Create an anonymous type with our values
+var queryParameters = new
+{
+    Height = 10,
+    Weight = 70,
+    BirthYear = 2000
+};
+
+// Build the base url
+var url = "https://localhost:5000/"
+    // Append the path
+    .AppendPathSegment("Customers")
+    // Set the querystring
+    .SetQueryParams(queryParameters);
+// Invoke the request
+await client.GetStringAsync(builder.Uri);
+
+
+// Build the base url
+url = new Url(new UriBuilder("https", "localhost", 5000, "Customers").Uri)
+   // Set the querystring
+   .SetQueryParams(queryParameters);
+// Invoke the request
+await client.GetStringAsync(url);
