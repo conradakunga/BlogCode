afb06b8210ba3093e3d1769f40e0953b9e58084b
Conrad Akunga <conradakunga@gmail.com>
2022-11-21 20:44:44 +0300

    Initial commit

---
+++ DeserializeNullString/Animal.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DeserializeNullString;
+
+public class NullToEmptyStringConverter : JsonConverter<string>
+{
+    // Override default null handling
+    public override bool HandleNull => true;
+
+    // Check the type
+    public override bool CanConvert(Type typeToConvert)
+    {
+        return typeToConvert == typeof(string);
+    }
+
+    public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        // Read the string
+        var res = reader.GetString();
+        // If it is am empty string, return a null
+        if (res == "")
+            return null;
+        else
+            // otherwise, return the read value
+            return res;
+    }
+
+    public override void Write(Utf8JsonWriter writer, string? value, JsonSerializerOptions options)
+    {
+        if (value == null)
+            writer.WriteStringValue("");
+        else
+            writer.WriteStringValue(value);
+    }
+}
+public record Animal
+{
+    // The animal name may be null
+    public string? Name { get; set; }
+
+    // The number of legs cannot be null
+    public int Legs { get; set; }
+}
---
+++ DeserializeNullString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using DeserializeNullString;
+using Serilog;
+var rawJson = """
+	{
+	  "Name": "",
+	  "Legs": 4
+	}
+	""";
+
+Log.Information("Serializing using default options");
+
+var retrievedMonkey = JsonSerializer.Deserialize<Animal>(rawJson);
+
+// create the options
+var options = new JsonSerializerOptions()
+{
+    WriteIndented = true
+};
+// register the converter
+options.Converters.Add(new NullToEmptyStringConverter());
+
+Log.Information("Serializing using custom options");
+
+retrievedMonkey = JsonSerializer.Deserialize<Animal>(rawJson, options);
+
+Console.ReadLine();
