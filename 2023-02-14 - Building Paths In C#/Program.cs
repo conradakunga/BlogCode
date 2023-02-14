3a4d30cbe5073029f323fc9e120fd48cba74ce31
Conrad Akunga <conradakunga@gmail.com>
2023-02-14 15:30:57 +0300

    Rename folders

---
+++ 2023-02-14 - Building Paths In C#/Program.cs
@@ -0,0 +1,17 @@
+﻿// Get the folder location
+var folderLocation = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\Incomplete";
+Console.WriteLine(folderLocation);
+// Create the folder
+Directory.CreateDirectory(folderLocation);
+
+folderLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Incomplete");
+Console.WriteLine(folderLocation);
+
+folderLocation = Path.Combine(folderLocation, "Active");
+Console.WriteLine(folderLocation);
+
+folderLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Incomplete", "Active", "2023", "January");
+Console.WriteLine(folderLocation);
+
+folderLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Incomplete", "Active", @"C:\2023", "January");
+Console.WriteLine(folderLocation);
