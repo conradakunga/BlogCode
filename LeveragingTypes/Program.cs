9b223d427d70c4c631e7014e11dee047b34974bc
Conrad Akunga <conradakunga@gmail.com>
2023-03-28 11:16:59 +0300

    Init Leveraging types & list patterns

---
+++ LeveragingTypes/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Transfer
+{
+    public string SrcAcc { get; set; }
+    public string TgtAcc { get; set; }
+    public string Txn_Dt { get; set; }
+    public string Val_Dt { get; set; }
+    public string Amt { get; set; }
+    public string Crcy { get; set; }
+    public string Prty { get; set; }
+    public string Chrg { get; set; }
+}
---
+++ ListPatterns/Program.cs
@@ -0,0 +1,33 @@
+﻿const string names = """
+Madonna
+Salma Hayek
+Robyn Fenty Rihanna
+Jennifer Lawrence Shrader
+Julia Louis-Dreyfus Scarlett Elizabeth 
+""";
+
+var nameList = names.Split(Environment.NewLine);
+foreach (var name in nameList)
+{
+    var person = name.Split(" ") switch
+    {
+        // Check if the string array has a single element
+        [var firstName] => new Actress() { Firstname = firstName },
+        // Check if the string array has two elements
+        [var firstName, var surname] => new Actress() { Firstname = firstName, Surname = surname },
+        // Check if the string array has three elements
+        [var firstName, var surname, var middleName] => new Actress() { Firstname = firstName, Surname = surname, Middlename = middleName },
+        // Catch all for greater than 3 names
+        [var firstName, var surname, .. var middleNames,] => new Actress() { Firstname = firstName, Surname = surname, Middlename = string.Join(" ", middleNames) },
+        _ => throw new Exception()
+    };
+    Console.WriteLine(person);
+}
+
+public record Actress
+{
+    // Firstname should never be null
+    public string Firstname { get; init; } = default!;
+    public string? Middlename { get; init; }
+    public string? Surname { get; init; }
+}
