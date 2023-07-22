650231fd962d6e3ef84f20a23684fa414155c6d3
Conrad Akunga <conradakunga@gmail.com>
2023-07-22 10:07:50 +0300

    Add passing test

--- 2023-07-22 - Request Headers/Client/HttpClientHeaderTests.cs
+++ 2023-07-22 - Request Headers/Client/HttpClientHeaderTests.cs
@@ -19,4 +19,15 @@ public class HttpClientHeaderTests
         client.DefaultRequestHeaders.Add("x-request-id", "foo");
         client.DefaultRequestHeaders.ToString().Should().StartWith("x-request-id: foo");
     }
+
+    [Fact]
+    public void Doctored_RequestID_Header_Is_Added_Correctly()
+    {
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.TryAddWithoutValidation("dummy", "dummy:dummy\r\nx-request-id: foo");
+        var headers = client.DefaultRequestHeaders.ToString();
+        headers.Should().StartWith("dummy", "dummy:dummy\r\nx-request-id: foo");
+        // There should be 3 headers - dummy,x-request-id and the automatically added host
+        headers.Split("\r\n").Length.Should().Be(3);
+    }
 }
