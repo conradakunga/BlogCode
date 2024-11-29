9fad20fff94169c219bd47f16bac3fc3a844f97b
Conrad Akunga <cakunga@innova.co.ke>
2024-11-29 04:07:35 +0300

    Tests for rounding

---
+++ NumberTruncation/RoundingTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+
+namespace NumberTruncation;
+
+public class RoundingTests
+{
+    [Theory]
+    [InlineData(123.449, 123.44)]
+    [InlineData(123.450, 123.45)]
+    [InlineData(123.451, 123.45)]
+    [InlineData(123.454, 123.45)]
+    [InlineData(123.455, 123.45)]
+    [InlineData(123.456, 123.45)]
+    [InlineData(123.459, 123.45)]
+    public void TruncationIsDoneCorrectly(decimal input, decimal expected)
+    {
+        Math.Round(input, 2, MidpointRounding.ToZero).Should().Be(expected);
+    }
+}
