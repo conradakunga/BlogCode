43bcfe3f94ba1a261141092f1b0633b16260306a
Conrad Akunga <cakunga@innova.co.ke>
2024-12-28 17:17:51 +0300

    Initial commit

---
+++ FileStore/FileStore/DiskFileStore.cs
@@ -0,0 +1,48 @@
+﻿namespace FileStore;
+
+public sealed class DiskFileStore
+{
+    private readonly string _fileStorePath;
+
+    public DiskFileStore(string fileStorePath)
+    {
+        _fileStorePath = fileStorePath;
+    }
+
+    public async Task<Guid> Upload(Stream fileStream, CancellationToken token)
+    {
+        // Generate a new identifier
+        var id = Guid.CreateVersion7();
+        // Write to disk
+        var fileStorePath = Path.Combine(_fileStorePath, id.ToString());
+        await using (var stream = new FileStream(fileStorePath, FileMode.Create))
+        {
+            await fileStream.CopyToAsync(stream, token);
+        }
+
+        return id;
+    }
+
+    public Task<bool> Exists(Guid id, CancellationToken token)
+    {
+        var fileStorePath = Path.Combine(_fileStorePath, id.ToString());
+        return Task.FromResult(File.Exists(fileStorePath));
+    }
+
+    public async Task<Stream> Download(Guid id, CancellationToken token)
+    {
+        // Build expected path of the file
+        var filePath = Path.Combine(_fileStorePath, id.ToString());
+
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException("File not found", filePath);
+        }
+
+        token.ThrowIfCancellationRequested();
+
+        // Return an async Stream
+        return await Task.FromResult(new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read, 4096,
+            true));
+    }
+}
---
+++ FileStore/FileStoreTests/DiskFileStoreTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using FileStore;
+using FluentAssertions;
+
+namespace FileStoreTests;
+
+public class DiskFileStoreTests
+{
+    [Fact]
+    public async Task File_Upload_And_Download_Is_Successful()
+    {
+        var store = new DiskFileStore(Path.GetTempPath());
+        // Create a new temp file with some known text
+        var testFile = Path.GetTempFileName();
+        var uploadData = Encoding.Default.GetBytes("This is some test data");
+        // Save the temp file
+        await File.WriteAllBytesAsync(testFile, uploadData);
+        // Upload the file
+        var id = await store.Upload(new FileStream(testFile, FileMode.Open), CancellationToken.None);
+
+        // Assert the ID is valid
+        id.Should().NotBeEmpty();
+
+
+        // Assert Exists works
+        var exists = await store.Exists(id, CancellationToken.None);
+        exists.Should().BeTrue();
+
+        // Download immediately
+        byte[] data;
+        using (var ms = new MemoryStream())
+        {
+            await (await store.Download(id, CancellationToken.None)).CopyToAsync(ms);
+            data = ms.ToArray();
+        }
+
+        // Assert data was returned
+        data.Should().NotBeEmpty();
+
+        // Assert the data hasn't changed
+        data.Should().BeEquivalentTo(uploadData);
+    }
+}
