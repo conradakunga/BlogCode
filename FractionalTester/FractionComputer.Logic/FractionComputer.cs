605df08bb0582e35a5b06f53c3e06c827b0d44a0
Conrad Akunga <conradakunga@gmail.com>
2025-01-10 22:17:11 +0300

    Init commit

---
+++ FractionalTester/FractionComputer.Logic/FractionComputer.cs
@@ -0,0 +1,11 @@
+﻿namespace PercentageComputer.Logic;
+
+public class FractionComputer
+{
+    public decimal Compute(decimal numerator, decimal denominator)
+    {
+        if (denominator == 0)
+            throw new ArgumentException("Denominator cannot be zero.");
+        return numerator / denominator;
+    }
+}
---
+++ FractionalTester/FractionComputer.Tests/FractionComputerTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using PercentageComputer.Logic;
+
+namespace PercentageComputer.Tests;
+
+public class FractionComputerTests
+{
+    [Theory]
+    [InlineData(50, 100, .5)]
+    [InlineData(20, 100, .20)]
+    [InlineData(40, 100, .4)]
+    [InlineData(0, 100, 0)]
+    [InlineData(200, 100, 2)]
+    [InlineData(1, 3, 0.33333)]
+    public void FractionIsComputedCorrectly(int numerator, int denominator, decimal fraction)
+    {
+        var computer = new FractionComputer();
+        //Math.Round(computer.Compute(numerator, denominator), 5).Should().Be(fraction);
+        computer.Compute(numerator, denominator).Should().BeApproximately(fraction, 0.00001M);
+    }
+}
