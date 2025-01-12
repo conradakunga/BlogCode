ce7c5e59bae74ce7395e3d4e7da18317e6c5227f
Conrad Akunga <conradakunga@gmail.com>
2025-01-12 13:16:55 +0300

    Refactor file

---
+++ OptionsVerification/OptionsVerification.Tests/ApplicationValidatorTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+
+namespace OptionsVerification.Tests;
+
+public class ApplicationValidatorTests
+{
+    [Fact]
+    public void Valid_Settings_Are_Validated()
+    {
+        var settings = new ApplicationOptions
+        {
+            APIKey = "ABCDEFGHIJ",
+            RetryCount = 3,
+            RequestsPerMinute = 3,
+            RequestsPerDay = 500
+        };
+
+        var validator = new ApplicationOptionsValidator();
+        var result = validator.Validate(settings);
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Invalid_RetryCount_Is_Validated()
+    {
+        var settings = new ApplicationOptions
+        {
+            APIKey = "ABCDEFGHIJ",
+            RetryCount = 5,
+            RequestsPerMinute = 5,
+            RequestsPerDay = 500
+        };
+
+        var validator = new ApplicationOptionsValidator();
+        var result = validator.Validate(settings);
+        // It should not be valid
+        result.IsValid.Should().BeFalse();
+        // Should have only one error
+        result.Errors.Should().HaveCount(1);
+        // Error message should be as follows
+        result.Errors[0].ErrorMessage.Should().Be("'Requests Per Minute' must be less than or equal to '3'.");
+    }
+}
