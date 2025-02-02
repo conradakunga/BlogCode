ac52e49b9e240f8dedd911819429669d4faff70e
Conrad Akunga <conradakunga@gmail.com>
2025-02-02 18:12:47 +0300

    Init commit

---
+++ LINQUnionBy/Agent.cs
@@ -0,0 +1,5 @@
+public sealed record Agent
+{
+    public required string Name { get; init; }
+    public required int Age { get; init; }
+}
---
+++ LINQUnionBy/Program.cs
@@ -0,0 +1,30 @@
+﻿Agent[] agents1980 =
+[
+    new() { Name = "James Bond", Age = 40 },
+    new() { Name = "Jason Bourne", Age = 37 },
+    new() { Name = "Evelyn Salt", Age = 28 },
+    new() { Name = "Ros Myers", Age = 25 },
+];
+
+
+Agent[] agents1990 =
+[
+    new() { Name = "James Bond", Age = 50 },
+    new() { Name = "Jason Bourne", Age = 45 },
+    new() { Name = "Evelyn Salt", Age = 35 },
+    new() { Name = "Ros Myers", Age = 30 },
+    new() { Name = "Ethan Hunt", Age = 40 },
+    new() { Name = "Benji Dunn", Age = 30 },
+    new() { Name = "Luther Stickell", Age = 37 },
+];
+
+// Union both lists
+var allAgents = agents1990.Union(agents1980).ToList();
+// Print to console
+//allAgents.ForEach(x => Console.WriteLine($"Agent: {x.Name}, Age: {x.Age}"));
+
+
+// Union both lists, using the Name for uniqueness
+allAgents = agents1990.UnionBy(agents1980, element => element.Name).ToList();
+// Print to console
+allAgents.ForEach(x => Console.WriteLine($"Agent: {x.Name}, Age: {x.Age}"));
