f430c5483490c5ece202e8714f603a9bf4e3f01f
Conrad Akunga <conradakunga@gmail.com>
2025-02-11 23:49:51 +0300

    Init commit

---
+++ EmptyStringTests/EmptyStringTests.cs
@@ -0,0 +1,51 @@
+﻿using Shouldly;
+
+namespace EmptyStringTests;
+
+public class EmptyStringTests
+{
+    public static IEnumerable<object[]> SampleStrings()
+    {
+        yield return [null]; // a null
+        yield return [""]; // a null string
+        yield return [" "]; // a space
+        yield return ["   "]; // a tab
+        yield return [Environment.NewLine]; // a newline
+    }
+
+    [Theory]
+    [MemberData(nameof(SampleStrings))]
+    public void NullCheck(string sample)
+    {
+        (sample == null).ShouldBe(true);
+    }
+
+
+    [Theory]
+    [MemberData(nameof(SampleStrings))]
+    public void NullStringCheck(string sample)
+    {
+        (sample == "").ShouldBe(true);
+    }
+
+    [Theory]
+    [MemberData(nameof(SampleStrings))]
+    public void IsNullOrEmpty(string sample)
+    {
+        string.IsNullOrEmpty(sample).ShouldBe(true);
+    }
+
+    [Theory]
+    [MemberData(nameof(SampleStrings))]
+    public void IsNullOrWhiteSpace(string sample)
+    {
+        string.IsNullOrWhiteSpace(sample).ShouldBe(true);
+    }
+
+    [Theory]
+    [MemberData(nameof(SampleStrings))]
+    public void TrimCheck(string sample)
+    {
+        (sample?.Trim().Length == 0).ShouldBe(true);
+    }
+}
