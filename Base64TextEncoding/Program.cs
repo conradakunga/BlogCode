ceb6fd379d4edcd44956b8663de1e8a46a4fbcd2
Conrad Akunga <conradakunga@gmail.com>
2025-02-12 22:15:50 +0300

    Update sample

--- Base64TextEncoding/Program.cs
+++ Base64TextEncoding/Program.cs
@@ -6,13 +6,13 @@ Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
     .CreateLogger();
 
-// // First get the text into a byte array
-// var textToEncode = "Hello";
-// // Encode the text
-// var rawBytes = Encoding.UTF8.GetBytes(textToEncode);
-// // Print to console
-// Log.Information("The encoded representation of {Original} is {Current}", textToEncode,
-//     Convert.ToBase64String(rawBytes));
+// First get the text into a byte array
+var textToEncode = "Hello";
+// Encode the text
+var rawByteArray = Encoding.UTF8.GetBytes(textToEncode);
+// Print to console
+Log.Information("The encoded representation of {Original} is {Current}", textToEncode,
+    Convert.ToBase64String(rawByteArray));
 
 // Get a ReadOnlySpan<byte> of the text
 var rawBytes = "Hello"u8;
