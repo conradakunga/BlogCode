27c1fa986cd793ac2af47105c43504c110952d9b
Conrad Akunga <conradakunga@gmail.com>
2025-02-12 22:15:10 +0300

    Init commit

---
+++ Base64TextEncoding/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Buffers.Text;
+using Serilog;
+using System.Text;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+// // First get the text into a byte array
+// var textToEncode = "Hello";
+// // Encode the text
+// var rawBytes = Encoding.UTF8.GetBytes(textToEncode);
+// // Print to console
+// Log.Information("The encoded representation of {Original} is {Current}", textToEncode,
+//     Convert.ToBase64String(rawBytes));
+
+// Get a ReadOnlySpan<byte> of the text
+var rawBytes = "Hello"u8;
+// Print to console
+Log.Information("The encoded representation of {Original} is {Current}", "Hello",
+    Convert.ToBase64String(rawBytes));
+Log.Information("The url encoded representation of {Original} is {Current}", "Hello",
+    Base64Url.EncodeToString(rawBytes));
