1663eb1d60647cd42877411fce8e33fb6ccd1b7d
Conrad Akunga <conradakunga@gmail.com>
2025-02-15 04:10:53 +0300

    Init commit

---
+++ ExecutingInfo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+// Setup our process start info
+var startInfo = new ProcessStartInfo
+{
+    FileName = "dotnet",
+    Arguments = "--version",
+    RedirectStandardOutput = true,
+    UseShellExecute = false,
+    CreateNoWindow = true
+};
+
+// Create a process
+var process = new Process();
+process.StartInfo = startInfo;
+
+// Start process
+process.Start();
+// Read output, and discard the newline
+string output = process.StandardOutput.ReadToEnd().Trim();
+process.WaitForExit();
+// Print 
+Console.WriteLine(output);
+
+// Use a simpler way
+Console.WriteLine(Environment.Version);
