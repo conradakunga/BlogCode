6c9fd4ef4db6ae2d985c8b2d22c731b26626b384
Conrad Akunga <conradakunga@gmail.com>
2025-02-16 14:56:34 +0300

    Init commit

---
+++ ValidateLinks/ValidateLinks.Core/ValidatedLink.cs
@@ -0,0 +1,9 @@
+﻿namespace ValidateLinks.Core;
+
+public sealed record ValidatedLink
+{
+    public required string Url { get; init; }
+    public required TimeSpan TimeToValidate { get; init; }
+    public required string Error { get; init; }
+    public bool Success => !string.IsNullOrEmpty(Error);
+}
---
+++ ValidateLinks/ValidateLinks.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ValidateLinks.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
