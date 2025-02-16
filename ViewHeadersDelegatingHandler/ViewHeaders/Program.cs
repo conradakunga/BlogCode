f00cc6b15406f0c5ea8280dbcf7fa902ed57535e
Conrad Akunga <conradakunga@gmail.com>
2025-02-16 21:05:42 +0300

    Init commit

---
+++ ViewHeadersDelegatingHandler/ViewHeaders/Program.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+using ViewHeaders;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+var client = new HttpClient(new ViewHeadersDelegatingHandler());
+// Set the user agent because some servers reject requests without one
+client.DefaultRequestHeaders.Add("User-Agent",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 14.5; rv:42.0) Gecko/20100101 Firefox/42.0");
+
+string[] urls =
+[
+    "https://www.booking.com",
+    "https://www.conradakunga.com",
+    "https://www.gigaom.com",
+    "https://www.newsblur.com",
+    "https://www.wordpress.com",
+];
+// Make a GET request
+foreach (var url in urls)
+{
+    _ = await client.GetAsync(url);
+}
---
+++ ViewHeadersDelegatingHandler/ViewHeadersDelegatingHandler/ViewHeadersDelegatingHandler.cs
@@ -0,0 +1,63 @@
+﻿using Serilog;
+
+namespace ViewHeaders;
+
+public sealed class ViewHeadersDelegatingHandler : DelegatingHandler
+{
+    public ViewHeadersDelegatingHandler() : base(new HttpClientHandler())
+    {
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        // Log request header summary
+        Log.Information("REQUEST HEADERS: {Count}", request.Headers.Count());
+        foreach (var header in request.Headers)
+        {
+            // Log each header
+            Log.Information("{Header} : {Value}", header.Key, string.Join(", ", header.Value));
+        }
+
+        // Check if the request contents have headers
+        if (request.Content?.Headers != null)
+        {
+            // Log request content header summary
+            Log.Information("REQUEST CONTENT HEADERS: {Count}", request.Content.Headers.Count());
+            foreach (var header in request.Content.Headers)
+            {
+                // Log each header
+                Log.Information("{Header} : {Value}", header.Key, string.Join(", ", header.Value));
+            }
+        }
+
+        Log.Information("Sending request to {URL}", request.RequestUri);
+
+        // Send the request and get the response
+        HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
+
+        // Log response headers summary
+        Log.Information("RESPONSE HEADERS: {Count}", response.Headers.Count());
+        foreach (var header in response.Headers)
+        {
+            // Log each header
+            Log.Information("{Header} : {Value}", header.Key, string.Join(", ", header.Value));
+        }
+
+        // Check if response content has headers. It is unlikely
+        // for the content to be null, but check anyway
+        if (response.Content?.Headers != null)
+        {
+            Log.Information("RESPONSE CONTENT HEADERS: {Count}", response.Content.Headers.Count());
+            foreach (var header in response.Content.Headers)
+            {
+                // Log each header
+                Log.Information("{Header} : {Value}", header.Key, string.Join(", ", header.Value));
+            }
+        }
+
+        Log.Information("Request complete");
+
+        return response;
+    }
+}
