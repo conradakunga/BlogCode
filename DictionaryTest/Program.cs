1d9e1b42daca297c0f7a2475de4682c09c15d12b
Conrad Akunga <conradakunga@gmail.com>
2025-02-23 07:11:07 +0300

    Init commit

---
+++ DictionaryTest/Program.cs
@@ -0,0 +1,21 @@
+﻿// Create a dictionary with a string key that stores integers
+
+var dict = new Dictionary<string, int>(StringComparer.CurrentCultureIgnoreCase);
+// Add an item to the dictionary
+dict.Add("James", 50);
+dict.Add("Eve", 25);
+dict.Add("Vesper", 35);
+
+// Print sample name to console
+Console.WriteLine(dict["James"]);
+// Capture input from user
+Console.WriteLine("Enter name of agent to search");
+
+var name = Console.ReadLine();
+// Print the value
+if (!string.IsNullOrEmpty(name))
+{
+    Console.WriteLine($"You entered {name} and we found {dict[name]}");
+}
+else
+    Console.WriteLine("Please enter a name");
