a6c39757eea56f1d34e151d7e34bc79686cdb2c6
Conrad Akunga <conradakunga@gmail.com>
2025-03-24 20:43:04 +0300

    Update

--- StateMachine/src/TrafficLight.Tests/TrafficLightTests.cs
+++ StateMachine/src/TrafficLight.Tests/TrafficLightTests.cs
@@ -1,13 +1,20 @@
 ﻿using FluentAssertions;
+using Microsoft.Extensions.Time.Testing;
 
 namespace StateMachine.Tests;
 
 public class TrafficLightTests
 {
-    [Fact]
-    public void Lights_Change_Correctly()
+    [Theory]
+    [InlineData(6, 00)]
+    [InlineData(12, 00)]
+    [InlineData(11, 59)]
+    public void Lights_Change_Correctly_During_The_Day(int hour, int minute)
     {
-        var light = new TrafficLight();
+        var currentDate = DateTime.Now;
+        var provider = new FakeTimeProvider();
+        provider.SetUtcNow(new DateTime(currentDate.Year, currentDate.Month, currentDate.Day, hour, minute, 0));
+        var light = new TrafficLight(provider);
         light.CurrentStatus.Should().Be(Status.Red);
         light.Transition();
         light.CurrentStatus.Should().Be(Status.Amber);
@@ -17,6 +24,28 @@ public class TrafficLightTests
         light.CurrentStatus.Should().Be(Status.Amber);
         light.Transition();
         light.CurrentStatus.Should().Be(Status.Red);
+        light.Transition();
+        light.CurrentStatus.Should().Be(Status.Amber);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(5, 59)]
+    public void Lights_Change_Correctly_During_The_Night(int hour, int minute)
+    {
+        var currentDate = DateTime.Now;
+        var provider = new FakeTimeProvider();
+        provider.SetUtcNow(new DateTimeOffset(currentDate.Year, currentDate.Month, currentDate.Day, hour, minute, 0,
+            TimeSpan.Zero));
+        var light = new TrafficLight(provider);
+        light.Transition();
+        light.CurrentStatus.Should().Be(Status.Amber);
+        light.Transition();
+        light.CurrentStatus.Should().Be(Status.Amber);
+        light.Transition();
+        light.CurrentStatus.Should().Be(Status.Amber);
+        light.Transition();
+        light.CurrentStatus.Should().Be(Status.Amber);
         light.Transition();
         light.CurrentStatus.Should().Be(Status.Amber);
     }
--- StateMachine/src/TrafficLigts/TrafficLight.cs
+++ StateMachine/src/TrafficLigts/TrafficLight.cs
@@ -1,36 +1,61 @@
+namespace StateMachine;
+
 public sealed class TrafficLight
 {
+    private readonly TimeProvider _provider;
+
     // Public property to retrieve the status
     public Status CurrentStatus { get; private set; }
 
     // Internal field to help with amber tracking
     private Status _priorStatus;
 
-    public TrafficLight()
+    public TrafficLight(TimeProvider provider)
     {
+        _provider = provider;
         CurrentStatus = Status.Red;
         _priorStatus = Status.Red;
     }
 
     public void Transition()
     {
-        if (CurrentStatus == Status.Red)
+        // Fetch the current time
+        var currentTime = TimeOnly.FromTimeSpan(_provider.GetLocalNow().TimeOfDay);
+        // Get midnight
+        var midnight = new TimeOnly(0, 0, 0);
+        // Get 6 AM
+        var sixAm = new TimeOnly(6, 0, 0);
+        // Check whether ot use night logic or day logic
+        if (currentTime >= midnight && currentTime < sixAm)
         {
-            _priorStatus = CurrentStatus;
-            CurrentStatus = Status.Amber;
+            // Transition to Amber, if not already
+            if (CurrentStatus != Status.Amber)
+            {
+                _priorStatus = CurrentStatus;
+                CurrentStatus = Status.Amber;
+            }
         }
-        else if (CurrentStatus == Status.Amber && _priorStatus == Status.Red)
+        else
         {
-            CurrentStatus = Status.Green;
-        }
-        else if (CurrentStatus == Status.Amber && _priorStatus == Status.Green)
-        {
-            CurrentStatus = Status.Red;
-        }
-        else if (CurrentStatus == Status.Green)
-        {
-            _priorStatus = CurrentStatus;
-            CurrentStatus = Status.Amber;
+            // Normal schedule
+            if (CurrentStatus == Status.Red)
+            {
+                _priorStatus = CurrentStatus;
+                CurrentStatus = Status.Amber;
+            }
+            else if (CurrentStatus == Status.Amber && _priorStatus == Status.Red)
+            {
+                CurrentStatus = Status.Green;
+            }
+            else if (CurrentStatus == Status.Amber && _priorStatus == Status.Green)
+            {
+                CurrentStatus = Status.Red;
+            }
+            else if (CurrentStatus == Status.Green)
+            {
+                _priorStatus = CurrentStatus;
+                CurrentStatus = Status.Amber;
+            }
         }
     }
 }
