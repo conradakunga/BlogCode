02f4e2277e9b31d51934d7d2f80c59d61fe538a1
Conrad Akunga <conradakunga@gmail.com>
2025-04-11 17:30:30 +0300

    Init commit

---
+++ Calculator/Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit.Abstractions;
+
+namespace Calculator.Tests;
+
+public class CalculatorTests
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public CalculatorTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Theory]
+    [ClassData(typeof(AdditionTestData))]
+    public void Integer_Addition_Is_Correct(int first, int second, int result)
+    {
+        var calc = new Calculator<int>();
+        _testOutputHelper.WriteLine($"Adding {first} + {second}");
+        calc.Add(first, second).Should().Be(result);
+    }
+
+    [Theory]
+    [ClassData(typeof(AdditionTestData))]
+    public void Decimal_Addition_Is_Correct(decimal first, decimal second, decimal result)
+    {
+        var calc = new Calculator<decimal>();
+        _testOutputHelper.WriteLine($"Adding {first} + {second}");
+        calc.Add(first, second).Should().Be(result);
+    }
+}
