27a704fff50e4b0a4f09605b557d1329b42566a8
Conrad Akunga <conradakunga@gmail.com>
2025-04-14 23:40:12 +0300

    Init checking

---
+++ NewLines/Program.cs
@@ -0,0 +1,33 @@
+﻿const string windows = "/Users/rad/WindowsShare/Windows.txt";
+const string osx = "/Users/rad/WindowsShare/OSX.txt";
+
+var windowsText = File.ReadAllText(windows);
+var osxText = File.ReadAllText(osx);
+
+// // Print all the lines from Windows
+// Console.WriteLine("WINDOWS");
+// foreach (var entry in windowsText.Split(Environment.NewLine))
+// {
+//     Console.WriteLine(entry);
+// }
+//
+// // Print all the lines from Windows
+// Console.WriteLine("OSX");
+// foreach (var entry in osxText.Split(Environment.NewLine))
+// {
+//     Console.WriteLine(entry);
+// }
+
+// Print all the lines from Windows
+Console.WriteLine("WINDOWS");
+foreach (var entry in windowsText.Split(Environment.NewLine).Select(x => new { Text = x, Length = x.Length }))
+{
+    Console.WriteLine($"Text:{entry.Text}, Length:{entry.Length}");
+}
+
+// Print all the lines from Windows
+Console.WriteLine("OSX");
+foreach (var entry in osxText.Split(Environment.NewLine).Select(x => new { Text = x.Trim(), Length = x.Length }))
+{
+    Console.WriteLine($"Text:{entry.Text}, Length:{entry.Length}");
+}
