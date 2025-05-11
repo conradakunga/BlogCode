4c70a34e408891a26e90f8405a70dcbf0f086d76
Conrad Akunga <conradakunga@gmail.com>
2025-05-11 18:34:14 +0300

    Init commit

---
+++ 2025-05-10 - Staging Environment/Program.cs
@@ -0,0 +1,25 @@
+using ApplicationSettings.Properties;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Check if we are running in development
+if (builder.Environment.IsDevelopment())
+{
+    // Load optional developer-specific settings
+    builder.Configuration.AddJsonFile($"appsettings.{Environment.MachineName}.json", optional: true);
+}
+
+// Configure options DI
+builder.Services.AddOptions<SystemSettings>()
+    .Bind(builder.Configuration.GetSection(nameof(SystemSettings)));
+
+var app = builder.Build();
+
+app.MapGet("/", (IOptions<SystemSettings> options) =>
+{
+    var settings = options.Value;
+    return settings;
+});
+
+app.Run();
