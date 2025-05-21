38ca996f1caf29f1ee874d6434330a2ef658a540
Conrad Akunga <conradakunga@gmail.com>
2025-05-22 00:11:26 +0300

    Init commit

---
+++ ServerInfo/HostInfo.cs
@@ -0,0 +1,10 @@
+public sealed class HostInfo
+{
+    public string host_platform { get; set; }
+    public string host_distribution { get; set; }
+    public string host_release { get; set; }
+    public string host_service_pack_level { get; set; }
+    public string host_sku { get; set; }
+    public string os_language_version { get; set; }
+    public string host_architecture { get; set; }
+}
