fe962ee17d642ca198a126299f04c9c1f4bc8a66
Conrad Akunga <conradakunga@gmail.com>
2025-05-24 22:16:24 +0300

    Init commit

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,51 @@
+﻿// {
+//     var numerator = 10;
+//     var denominator = 0;
+//     var result = numerator / denominator;
+// }
+
+{
+    try
+    {
+        var numerator = 10;
+        var denominator = 0;
+        var result = numerator / denominator;
+    }
+    catch (DivideByZeroException)
+    {
+        Console.WriteLine("You attempted to divide by zero. Please try again.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Some other exception occured: {ex.Message}");
+    }
+}
+
+{
+    var result = Add(1, 3);
+
+    try
+    {
+        // Call the recursive method
+        Recursive();
+    }
+    catch (StackOverflowException)
+    {
+        Console.WriteLine("Stack overflow");
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Unexpected exception");
+    }
+
+// A recursive method that calls itself
+    void Recursive()
+    {
+        Recursive();
+    }
+
+    int Add(int first, int second)
+    {
+        return first + second;
+    }
+}
