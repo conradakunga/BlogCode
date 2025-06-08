e536adad825140d37e863acd5e33b6181f280f43
Conrad Akunga <conradakunga@gmail.com>
2025-06-08 13:58:39 +0300

    Init commit

---
+++ OptimizeDictionaryAdd/Program.cs
@@ -0,0 +1,57 @@
+﻿// Define dictionary
+
+{
+    var numbers = new Dictionary<int, string>();
+    // Add items
+    numbers.Add(1, "One");
+    numbers.Add(2, "Two");
+    numbers.Add(3, "Three");
+    numbers.Add(4, "Four");
+    numbers.Add(5, "Five");
+
+    // Add an item with an existing key
+    // numbers.Add(5, "Six");
+    if (!numbers.ContainsKey(5))
+        numbers.Add(5, "Six");
+
+    // Print items
+    foreach (var element in numbers)
+    {
+        Console.WriteLine($"Key: {element.Key} ; Value: {element.Value}");
+    }
+}
+
+{
+    var numbers = new Dictionary<int, string>();
+    //Add items
+    numbers[1] = "One";
+    numbers[2] = "Two";
+    numbers[3] = "Three";
+    numbers[4] = "Four";
+    numbers[5] = "Five";
+    // Update existing key
+    numbers[5] = "Six";
+    // Print items
+    foreach (var element in numbers)
+    {
+        Console.WriteLine($"Key: {element.Key} ; Value: {element.Value}");
+    }
+}
+{
+    var numbers = new Dictionary<int, string>();
+    // Add items
+    numbers.Add(1, "One");
+    numbers.Add(2, "Two");
+    numbers.Add(3, "Three");
+    numbers.Add(4, "Four");
+    numbers.Add(5, "Five");
+
+    if (!numbers.TryAdd(5, "Six"))
+        Console.WriteLine("Could not add item with index 5 as it already exists!");
+
+    // Print items
+    foreach (var element in numbers)
+    {
+        Console.WriteLine($"Key: {element.Key} ; Value: {element.Value}");
+    }
+}
