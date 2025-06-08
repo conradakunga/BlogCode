522f743e0a6c8736849ff1b6a4c08483f88a8c66
Conrad Akunga <conradakunga@gmail.com>
2025-06-08 14:16:27 +0300

    Init commit

---
+++ 2025-06-08 - TryGet/Program.cs
@@ -0,0 +1,36 @@
+﻿// Define dictionary
+
+{
+    var numbers = new Dictionary<int, string>
+    {
+        // Add items
+        { 1, "One" },
+        { 2, "Two" },
+        { 3, "Three" },
+        { 4, "Four" },
+        { 5, "Five" }
+    };
+
+    var itemValue = numbers[5];
+
+    Console.WriteLine(itemValue);
+
+    // itemValue = numbers[6];
+
+    if (numbers.ContainsKey(6))
+    {
+        itemValue = numbers[6];
+        Console.WriteLine(itemValue);
+    }
+    else
+        Console.WriteLine("Key 6 does not exist");
+    
+    
+    if (numbers.TryGetValue(6, out var number))
+    {
+        itemValue = number;
+        Console.WriteLine(itemValue);
+    }
+    else
+        Console.WriteLine("Key 6 does not exist");
+}
