230ea3f1a2aee55a17faa0e8ff21e6f0e2364a4d
Conrad Akunga <conradakunga@gmail.com>
2025-06-16 21:06:31 +0300

    Init commit

---
+++ 2025-06-15 - TryParse/Program.cs
@@ -0,0 +1,35 @@
+﻿// Console.WriteLine("Enter first number:");
+// var firstInput = Console.ReadLine();
+// Console.WriteLine("Enter second number:");
+// var secondInput = Console.ReadLine();
+//
+// int first, second;
+// try
+// {
+//     first = int.Parse(firstInput);
+// }
+// catch (Exception e)
+// {
+//     Console.WriteLine("Invalid first number");
+//     return;
+// }
+//
+// try
+// {
+//     second = int.Parse(secondInput);
+// }
+// catch (Exception e)
+// {
+//     Console.WriteLine("Invalid second number");
+//     return; 
+// }
+//
+// Console.WriteLine($"The result is: {first + second}");
+
+int first, second;
+Console.WriteLine("Enter first number:");
+while (!int.TryParse(Console.ReadLine(), out first)) ;
+Console.WriteLine("Enter second number:");
+while (!int.TryParse(Console.ReadLine(), out second)) ;
+
+Console.WriteLine($"The result is: {first + second}");
