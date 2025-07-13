051e3a78accee8c2451c5e26a0aa2041b1448ef7
Conrad Akunga <conradakunga@gmail.com>
2025-07-13 19:32:33 +0300

    Init commit

---
+++ RabbitMQDeleteQueue/Program.cs
@@ -0,0 +1,36 @@
+﻿using EasyNetQ.Management.Client;
+using Serilog;
+
+async Task Main()
+{
+    // Configure logging
+    Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console()
+        .CreateLogger();
+
+    // Fetch / configure access parameters
+    var username = "test";
+    var password = "test";
+    var hostAddress = "localhost";
+    var adminPort = 15672;
+    var queueName = "ClientUnitizationResponse_HERACLES";
+
+    Log.Information("Connecting to {Host} on Port {Port}", hostAddress, adminPort);
+    // Create a management client
+    var mc = new ManagementClient(new Uri($"http://{hostAddress}:{adminPort}"), username, password);
+    try
+    {
+        // Fetch the queue form the default vHost
+        var queue = await mc.GetQueueAsync("/", queueName);
+
+        // Delete the queue
+        Log.Information("Deleting {Queue} ...", queue.Name);
+        await mc.DeleteQueueAsync(queue.Vhost, queue.Name);
+
+        Log.Information("Deleted {Queue}", queue.Name);
+    }
+    catch (UnexpectedHttpStatusCodeException ex)
+    {
+        Log.Error(ex, "Could not find queue {Queue}", queueName);
+    }
+}
