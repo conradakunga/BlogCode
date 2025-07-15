6840fc2497e9e3f9b179201488eee223bd6d45a4
Conrad Akunga <conradakunga@gmail.com>
2025-07-15 22:45:53 +0300

    Init commit

---
+++ AppDelays/Program.cs
@@ -0,0 +1,33 @@
+﻿using Serilog;
+
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+
+// // Start fake task
+// Log.Information("Starting a long task");
+// // Sleep for 10 seconds synchronously
+// Thread.Sleep(TimeSpan.FromSeconds(10));
+// Log.Information("Completed long task");
+//
+
+// // Start fake task
+// Log.Information("Starting a long task");
+// // Sleep for 10 seconds asynchronously
+// await Task.Delay(TimeSpan.FromSeconds(10));
+// Log.Information("Completed long task");
+
+// // Start fake task
+// Log.Information("Starting a long task");
+// // Call complex method, requesting a 10 second delay 
+// // and pass a cancellation token
+
+Log.Information("Starting a long task");
+// Create a cancellation token source
+var cts = new CancellationTokenSource();
+// Invoke method, and pass delay and token
+await ComplexOperation(10, cts.Token);
+Log.Information("Completed long task");
+
+async Task ComplexOperation(int delayInSeconds, CancellationToken token)
+{
+    await Task.Delay(TimeSpan.FromSeconds(delayInSeconds), token);
+}
