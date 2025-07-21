247cd55df2dd2b195a2f534d6051d31a70631e21
Conrad Akunga <conradakunga@gmail.com>
2025-07-21 20:02:01 +0300

    Rename folders

---
+++ 2025-07-21 - Sending Via Google Cloud/Program.cs
@@ -0,0 +1,55 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using Microsoft.Graph.Users.Item.SendMail;
+
+const string applicationID = "APPLICATION_ID";
+const string tenantID = "TENANT_ID";
+const string secretValue = "CLIENT_SECRET";
+const string fromAddress = "cakunga@innova.co.ke";
+const string toAddress = "conradakunga@gmail.com";
+
+try
+{
+    // Set our scopes to the default
+    var scopes = new[] { "https://graph.microsoft.com/.default" };
+
+    // Create TokenCredential
+    var credential = new ClientSecretCredential(tenantID, applicationID, secretValue);
+
+    // Create Graph client
+    var graphClient = new GraphServiceClient(credential, scopes);
+
+    // Build email
+    var message = new Message
+    {
+        Subject = "Test Email",
+        Body = new ItemBody
+        {
+            ContentType = BodyType.Text,
+            Content = "Test email"
+        },
+        ToRecipients =
+        [
+            new Recipient
+            {
+                EmailAddress = new EmailAddress
+                {
+                    Address = toAddress
+                }
+            }
+        ]
+    };
+
+    // Send email
+    await graphClient.Users[fromAddress].SendMail.PostAsync(new SendMailPostRequestBody
+    {
+        Message = message
+    });
+    
+    Console.WriteLine("Message sent");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
