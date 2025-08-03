92d02183cee70c1bae458d06bcf61d04eb84aafa
Conrad Akunga <conradakunga@gmail.com>
2025-08-03 15:08:39 +0300

    Init commit

---
+++ ListManipulation/Program.cs
@@ -0,0 +1,22 @@
+﻿List<int> list = [0, 1, 2, 3, 4, 5];
+
+// // Print elements
+// list.ForEach(Console.Write);
+// Console.WriteLine();
+// // Sort the list
+// list.Reverse();
+// // Print elements
+// list.ForEach(Console.Write);
+// Console.WriteLine();
+//
+
+list = [0, 1, 2, 3, 4, 5];
+// Print elements
+list.ForEach(Console.Write);
+Console.WriteLine();
+// Sort the list
+list.OrderByDescending(x => x).ToList().ForEach(Console.Write);
+Console.WriteLine();
+// Print elements
+list.ForEach(Console.Write);
+Console.WriteLine();
