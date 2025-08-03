a128bec8f556fa190583355229465e45a0cc3859
Conrad Akunga <conradakunga@gmail.com>
2025-08-03 21:19:01 +0300

    Init commit

---
+++ RandomStringGenerator/RandomStringGenerator/RandomStringGenerator.cs
@@ -0,0 +1,15 @@
+﻿namespace Generator;
+
+public static class RandomStringGenerator
+{
+    public static string GenerateRandomString(int length)
+    {
+        // Ensure length is at least 1
+        ArgumentOutOfRangeException.ThrowIfLessThan(length, 1);
+
+        // Generate string
+        return new string(Enumerable.Range(0, length)
+            .Select(_ => Constants.Alphabet[Random.Shared.Next(Constants.Alphabet.Length)])
+            .ToArray());
+    }
+}
---
+++ RandomStringGenerator/RandomStringGeneratorTests/RandomStringGeneratorTests.cs
@@ -0,0 +1,38 @@
+﻿using AwesomeAssertions;
+using Generator;
+using Xunit.Abstractions;
+
+namespace RandomStringGeneratorTests;
+
+public class RandomStringGeneratorTests
+{
+    private readonly ITestOutputHelper _output;
+
+    public RandomStringGeneratorTests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void InvalidLengthsRejected(int length)
+    {
+        var ex = Record.Exception(() => RandomStringGenerator.GenerateRandomString(length));
+        ex.Should().BeOfType<ArgumentOutOfRangeException>();
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(5)]
+    [InlineData(20)]
+    [InlineData(50)]
+    public void StringsAreGeneratedCorrectly(int length)
+    {
+        var randomString = RandomStringGenerator.GenerateRandomString(length);
+        randomString.Length.Should().Be(length);
+        _output.WriteLine($"Generated string: {randomString} of length {length}");
+
+        Constants.Alphabet.ToCharArray().Should().Contain(randomString);
+    }
+}
