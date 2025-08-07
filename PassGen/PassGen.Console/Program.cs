baf8934d984877eac2186006400fdc127134865a
Conrad Akunga <conradakunga@gmail.com>
2025-08-07 17:13:44 +0300

    Add command line support

--- PassGen/PassGen.Console/Program.cs
+++ PassGen/PassGen.Console/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PassGen;
+using Spectre.Console;
+
+var passwordLength = AnsiConsole.Prompt(
+    new TextPrompt<int>("How long should the password be?"));
+var numbers = AnsiConsole.Prompt(
+    new TextPrompt<int>("How many numbers should the password contain?"));
+var symbols = AnsiConsole.Prompt(
+    new TextPrompt<int>("How many symbols should the password contain?"));
+
+AnsiConsole.MarkupLineInterpolated(
+    $"Generating password of length {passwordLength} with {numbers} numbers and {symbols}...");
+
+try
+{
+    var password = PasswordGenerator.GeneratePassword(numbers, symbols, passwordLength);
+
+    AnsiConsole.MarkupLineInterpolated($"The generated password is [bold red]{password}[/]");
+}
+catch (Exception ex)
+{
+    AnsiConsole.MarkupLineInterpolated($"[bold red]Error generating password: {ex.Message}[/]");
+}
