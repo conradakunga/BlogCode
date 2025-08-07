01cf9c966060b6f1748adf747a7e657de2328048
Conrad Akunga <conradakunga@gmail.com>
2025-08-07 18:36:55 +0300

    Put back original simple UI

--- PassGen/PassGen.Console/Program.cs
+++ PassGen/PassGen.Console/Program.cs
@@ -6,6 +6,32 @@ using TextCopy;
 var app = new CommandApp<GeneratePasswordCommand>();
 return app.Run(args);
 
+//
+// The initial code
+//
+// var passwordLength = AnsiConsole.Prompt(
+//     new TextPrompt<int>("How long should the password be?"));
+//
+// var numbers = AnsiConsole.Prompt(
+//     new TextPrompt<int>("How many numbers should the password contain?"));
+//
+// var symbols = AnsiConsole.Prompt(
+//     new TextPrompt<int>("How many symbols should the password contain?"));
+//
+// AnsiConsole.MarkupLineInterpolated(
+//     $"Generating password of length {passwordLength} with {numbers} numbers and {symbols}...");
+// try
+// {
+//     var password = PasswordGenerator.GeneratePassword(numbers, symbols, passwordLength);
+//
+//     AnsiConsole.MarkupLineInterpolated($"The generated password is [bold red]{password}[/]");
+// }
+// catch
+//     (Exception ex)
+// {
+//     AnsiConsole.MarkupLineInterpolated($"[bold red]Error generating password: {ex.Message}[/]");
+// }
+
 public class GeneratePasswordCommand : Command<PasswordSettings>
 {
     public override int Execute(CommandContext context, PasswordSettings settings)
