6b6d2b8c7faf2fd2c8e8e1469542e6e3a64fd12a
Conrad Akunga <conradakunga@gmail.com>
2025-08-13 22:28:38 +0300

    Init commit

---
+++ GenericListStore/GenericListStore/GenericListStore.cs
@@ -0,0 +1,36 @@
+﻿namespace GenericListStore;
+
+public class GenericListStore
+{
+    private readonly Dictionary<string, object> _dictLists;
+
+    /// <summary>
+    /// Lists held
+    /// </summary>
+    public int Count => _dictLists.Count;
+
+    public GenericListStore()
+    {
+        _dictLists = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+    }
+
+    public void Add<T>(string name, List<T> list)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(nameof(list));
+
+        if (!_dictLists.TryAdd(name, list))
+            throw new ArgumentException($"A list with the name '{name}' already exists.", nameof(name));
+    }
+
+    // Get the list with the correct type
+    public List<T> Get<T>(string name)
+    {
+        if (!_dictLists.TryGetValue(name, out var list))
+            throw new KeyNotFoundException($"No list found with the name '{name}'.");
+
+        if (list is List<T> typedList)
+            return typedList;
+
+        throw new InvalidCastException($"List '{name}' does not store {typeof(T).Name} items");
+    }
+}
---
+++ GenericListStore/GenericListStoreTests/UnitTest1.cs
@@ -0,0 +1,83 @@
+﻿using AwesomeAssertions;
+
+namespace GenericListStoreTests;
+
+using GenericListStore;
+
+public class GenericListStoreTests
+{
+    [Fact]
+    public void Store_Should_Initialize_Empty()
+    {
+        var store = new GenericListStore();
+        store.Count.Should().Be(0);
+    }
+
+    [Fact]
+    public void Store_Should_Store_Items_Correctly()
+    {
+        List<string> items = ["one", "two", "three"];
+        var store = new GenericListStore();
+        store.Add("strings", items);
+        // Fetch items
+        var returned = store.Get<string>("strings");
+        returned.Should().BeEquivalentTo(items);
+    }
+
+    [Fact]
+    public void Store_Should_Store_Multiple_Type_Items_Correctly()
+    {
+        List<string> stringItems = ["one", "two", "three"];
+        List<int> intItems = [1, 2, 3, 4];
+
+        var store = new GenericListStore();
+
+        const string stringKey = "strings";
+        const string intKey = "ints";
+
+        store.Add(stringKey, stringItems);
+        store.Add(intKey, intItems);
+
+        // Fetch items
+        var returnedStrings = store.Get<string>(stringKey);
+        var returnedInts = store.Get<int>(intKey);
+        returnedStrings.Should().BeEquivalentTo(stringItems);
+        returnedInts.Should().BeEquivalentTo(intItems);
+    }
+
+    [Fact]
+    public void Store_Should_Reject_Duplicate_Name()
+    {
+        List<string> first = ["one", "two", "three"];
+        List<string> second = ["four", "five", "six"];
+
+        var store = new GenericListStore();
+        store.Add("strings", first);
+        var ex = Record.Exception(() => store.Add("strings", second));
+        ex.Should().BeOfType<ArgumentException>();
+    }
+
+    [Fact]
+    public void Store_Should_Throw_Exception_On_Invalid_Cast()
+    {
+        List<string> first = ["one", "two", "three"];
+
+        var store = new GenericListStore();
+        store.Add("strings", first);
+
+        var ex = Record.Exception(() => store.Get<int>("strings"));
+        ex.Should().BeOfType<InvalidCastException>();
+    }
+
+    [Fact]
+    public void Store_Should_Throw_Exception_For_Invalid_Name()
+    {
+        List<string> first = ["one", "two", "three"];
+
+        var store = new GenericListStore();
+        store.Add("strings", first);
+
+        var ex = Record.Exception(() => store.Get<int>("DOESNOTEXIST"));
+        ex.Should().BeOfType<KeyNotFoundException>();
+    }
+}
