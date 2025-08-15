ba12d6605a20717e204df570c2af88f4eb1ab315
Conrad Akunga <conradakunga@gmail.com>
2025-08-15 09:35:38 +0300

    Update documentation

--- PassGen/Tests/RepeatAttribute.cs
+++ PassGen/Tests/RepeatAttribute.cs
@@ -3,10 +3,18 @@ using Xunit.Sdk;
 
 namespace Tests;
 
+/// <summary>
+/// Runs tests multiple times
+/// </summary>
 public sealed class RepeatAttribute : DataAttribute
 {
     private readonly int _count;
 
+    /// <summary>
+    /// Number of times to run the tests
+    /// </summary>
+    /// <param name="count"></param>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
     public RepeatAttribute(int count)
     {
         if (count < 1)
@@ -18,6 +26,11 @@ public sealed class RepeatAttribute : DataAttribute
         _count = count;
     }
 
+    /// <summary>
+    /// Returns an enumerable to be used by the test runner
+    /// </summary>
+    /// <param name="_"></param>
+    /// <returns></returns>
     public override IEnumerable<object[]> GetData(MethodInfo _)
     {
         foreach (var iteration in Enumerable.Range(0, _count))
