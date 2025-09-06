c3ea5b4bea3fd875bbca29c00590b88b73734296
Conrad Akunga <conradakunga@gmail.com>
2025-09-06 11:41:27 +0300

    Init commit

---
+++ RedisVersion/Program.cs
@@ -0,0 +1,18 @@
+﻿// Adjust your connection string as needed
+
+using StackExchange.Redis;
+
+// Get a connection to the Redis server
+await using (var connection =
+             await ConnectionMultiplexer.ConnectAsync("localhost:6379,password=YourStrongPassword123,allowAdmin=true"))
+{
+    // Retrieve the server
+    var server = connection.GetServer("localhost", 6379);
+
+    // Execute the INFO command
+    var version = server.Info("Server")
+        .SelectMany(g => g)
+        .FirstOrDefault(p => p.Key == "redis_version").Value;
+
+    Console.WriteLine($"Redis Version: {version}");
+}
