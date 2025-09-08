b018253e3886566a93f1698a1f34e16dcaef6778
Conrad Akunga <conradakunga@gmail.com>
2025-09-08 21:15:33 +0300

    Rename files

--- 2025-09-06 - ElasticSearchVersion/Program.cs
+++ 2025-09-06 - ElasticSearchVersion/Program.cs
@@ -1,21 +1,20 @@
-﻿// using System.Net.Http.Json;
-// using ElasticSearchVersion;
-//
-// var client = new HttpClient();
-// const string elasticSearchRoot = "http://localhost:9200/";
-//
-// var response = await client.GetFromJsonAsync<ElasticSearchResponse>(elasticSearchRoot);
-// Console.WriteLine(response!.Version.Number);
-
+﻿using System.Net.Http.Json;
+using ElasticSearchVersion;
 using Elastic.Clients.Elasticsearch;
-using Elastic.Transport;
+
+var client = new HttpClient();
+const string elasticSearchRoot = "http://localhost:9200/";
+
+var response = await client.GetFromJsonAsync<ElasticSearchResponse>(elasticSearchRoot);
+Console.WriteLine(response!.Version.Number);
+
 
 var settings = new ElasticsearchClientSettings(new Uri("http://localhost:9200"));
-    // .Authentication(new BasicAuthentication("elastic", "YourPassword"))
-    // .ServerCertificateValidationCallback((o, c, ch, e) => true); // ignore SSL for testing
+// .Authentication(new BasicAuthentication("elastic", "YourPassword"))
+// .ServerCertificateValidationCallback((o, c, ch, e) => true); // ignore SSL for testing
 
-var client = new ElasticsearchClient(settings);
+var elasticsearchClient = new ElasticsearchClient(settings);
 
-var info = await client.InfoAsync();
+var info = await elasticsearchClient.InfoAsync();
 
 Console.WriteLine($"Version: {info.Version.Number}");
