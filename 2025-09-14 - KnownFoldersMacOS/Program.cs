f2a31fc560d9569cce2c46a486bc0fada1512540
Conrad Akunga <conradakunga@gmail.com>
2025-09-15 23:21:37 +0300

    Minor cleanup

---
+++ 2025-09-14 - KnownFoldersMacOS/Program.cs
@@ -0,0 +1,14 @@
+﻿// Get the home location
+
+string homeLocation = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+// Build the download path
+string downloadLocation = Path.Combine(homeLocation, "Downloads");
+// Check that the location exists
+if (Directory.Exists(downloadLocation))
+{
+    Console.WriteLine($"The Downloads location is '{downloadLocation}'");
+}
+else
+{
+    Console.WriteLine("Could not find Downloads location");
+}
