c5e979ec05e21739203812b47fde44b841e56c8e
Conrad Akunga <conradakunga@gmail.com>
2025-09-22 11:02:59 +0300

    Init commit

---
+++ DigitalSensor/DigitalSensorLogic/Sensor.cs
@@ -0,0 +1,61 @@
+﻿namespace V1
+{
+    public class Sensor
+    {
+        public int Display { get; }
+
+        public Sensor(int input)
+        {
+            Display = input;
+        }
+    }
+}
+
+namespace V2
+{
+    public class Sensor
+    {
+        public int Display { get; }
+
+        public Sensor(int input)
+        {
+            if (input < 0)
+                Display = 0;
+            else if (input > 10)
+                Display = 10;
+            else
+                Display = input;
+        }
+    }
+}
+
+namespace V3
+{
+    public class Sensor
+    {
+        public int Display { get; }
+
+        public Sensor(int input)
+        {
+            Display = input switch
+            {
+                < 0 => 0,
+                > 10 => 10,
+                _ => input
+            };
+        }
+    }
+}
+
+namespace V4
+{
+    public class Sensor
+    {
+        public int Display { get; }
+
+        public Sensor(int input)
+        {
+            Display = Math.Clamp(input, 0, 10);
+        }
+    }
+}
---
+++ DigitalSensor/DigitalSensorTests/SensorTests.cs
@@ -0,0 +1,19 @@
+﻿using AwesomeAssertions;
+using V4;
+
+namespace DigitalSensorTests;
+
+public class SensorTests
+{
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(-1, 0)]
+    [InlineData(5, 5)]
+    [InlineData(10, 10)]
+    [InlineData(11, 10)]
+    public void Test1(int input, int expected)
+    {
+        var sensor = new Sensor(input);
+        sensor.Display.Should().Be(expected);
+    }
+}
