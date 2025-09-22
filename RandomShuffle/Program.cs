1fff4bc5c1e73a1deaebb0e1cd843aa8df2aed98
Conrad Akunga <conradakunga@gmail.com>
2025-09-22 12:51:05 +0300

    Init commit

---
+++ RandomShuffle/Program.cs
@@ -0,0 +1,21 @@
+﻿var numbers = Enumerable.Range(1, 25).ToArray();
+
+// Sort using random order method
+
+var shuffled = numbers.OrderBy(n => Random.Shared.Next()).ToArray();
+
+foreach (var number in shuffled)
+{
+    Console.Write($"{number} ");
+}
+
+Console.WriteLine();
+
+// Shuffle in place
+
+Random.Shared.Shuffle(numbers);
+
+foreach (var number in numbers)
+{
+    Console.Write($"{number} ");
+}
