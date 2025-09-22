d655bea585cc88e7d2f2bb564d16d7e52a44559c
Conrad Akunga <conradakunga@gmail.com>
2025-09-22 22:45:23 +0300

    Rename folders

---
+++ 2025-09-21 - RandomSample/Program.cs
@@ -0,0 +1,18 @@
+﻿var numbers = Enumerable.Range(1, 25).ToArray();
+
+var shuffled = numbers.OrderBy(n => Random.Shared.Next()).Take(10).ToArray();
+
+foreach (var number in shuffled)
+{
+    Console.Write($"{number} ");
+}
+
+Console.WriteLine();
+
+var sampled = Random.Shared.GetItems(numbers, 10);
+
+foreach (var number in sampled)
+{
+    Console.Write($"{number} ");
+}
+
