06486ae5ca85d87a4839fc0f3c864e28e42b515d
Conrad Akunga <conradakunga@gmail.com>
2025-09-28 18:37:08 +0300

    Update code

--- 2025-09-26 - RandomULong/Program.cs
+++ 2025-09-26 - RandomULong/Program.cs
@@ -1,2 +1,7 @@
-﻿var randomuInt = (uint)Random.Shared.NextInt64(uint.MinValue, ((long)uint.MaxValue) + 1);
-Console.WriteLine(randomuInt);
+﻿// Create a 64 bit array
+var randomNumberBytes = new byte[8];
+// Fill with random bytes
+Random.Shared.NextBytes(randomNumberBytes);
+// Convert to an unsinged int64 (long)
+var randomNumber = BitConverter.ToUInt64(randomNumberBytes);
+Console.WriteLine(randomNumber);
