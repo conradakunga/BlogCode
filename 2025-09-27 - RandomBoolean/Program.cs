6f131cc19b1e51558289c3dd9abaafde11b49921
Conrad Akunga <conradakunga@gmail.com>
2025-09-28 19:17:44 +0300

    initial commit

---
+++ 2025-09-27 - RandomBoolean/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography;
+
+// Create a single random byte buffer
+Span<byte> buffer = stackalloc byte[1];
+// Fill buffer with the random number generator
+RandomNumberGenerator.Fill(buffer);
+// Use the lowest bit to determine true or false
+var randomBoolean = (buffer[0] & 1) == 1;
+
+// Output
+Console.WriteLine(randomBoolean);
