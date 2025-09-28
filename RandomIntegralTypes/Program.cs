a796a4a30989aac557fee2187b96a0ba70cd0769
Conrad Akunga <conradakunga@gmail.com>
2025-09-28 17:49:09 +0300

    Init commit

---
+++ RandomIntegralTypes/Program.cs
@@ -0,0 +1,16 @@
+﻿// byte
+var randomByte = (byte)Random.Shared.Next(byte.MinValue, byte.MaxValue);
+Console.WriteLine(randomByte);
+
+// Singed byte
+var randomsByte = (sbyte)Random.Shared.Next(sbyte.MinValue, sbyte.MaxValue + 1);
+Console.WriteLine(randomsByte);
+
+// Short
+var randomShort = (short)Random.Shared.Next(short.MinValue, short.MaxValue + 1);
+Console.WriteLine(randomShort);
+
+// Unsinged short
+var randomuShort = (ushort)Random.Shared.Next(ushort.MinValue, ushort.MaxValue + 1);
+
+Console.WriteLine(randomuShort);
