52ee4a8ced08d11ece9e95179ad07a2c4167aeb9
Conrad Akunga <conradakunga@gmail.com>
2025-10-01 21:28:56 +0300

    Add logic to list calendars

--- CorrectDaysInMonth/Program.cs
+++ CorrectDaysInMonth/Program.cs
@@ -8,3 +8,20 @@ Console.WriteLine($"{dateOnly:MMMM yyyy} has {daysInMonth} days in the Gregorian
 var hijri = new HijriCalendar();
 daysInMonth = hijri.GetDaysInMonth(dateOnly.Year, dateOnly.Month);
 Console.WriteLine($"{dateOnly:MMMM yyyy} has {daysInMonth} days in the Hijri Calendar");
+
+var calendar = typeof(Calendar);
+// Get the assembly
+var calendars = calendar.Assembly
+    // Get the types in the assembly
+    .GetTypes()
+    // Filter out the non-abstract classes, and get those that are assignable
+    .Where(t => !t.IsAbstract && calendar.IsAssignableFrom(t))
+    // Order by name
+    .OrderBy(t => t.Name);
+
+Console.WriteLine("Calendars:");
+
+foreach (var type in calendars!)
+{
+    Console.WriteLine($"- {type.Name}");
+}
