b8d0ed10d93be38d625e7739662e7c41305b47a7
Conrad Akunga <conradakunga@gmail.com>
2025-10-01 21:12:04 +0300

    Init commit

---
+++ CorrectDaysInMonth/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+var dateOnly = new DateOnly(2000, 1, 1);
+var gregorian = new GregorianCalendar();
+var daysInMonth = gregorian.GetDaysInMonth(dateOnly.Year, dateOnly.Month);
+Console.WriteLine($"{dateOnly:MMMM yyyy} has {daysInMonth} days in the Gregorian Calendar");
+
+var hijri = new HijriCalendar();
+daysInMonth = hijri.GetDaysInMonth(dateOnly.Year, dateOnly.Month);
+Console.WriteLine($"{dateOnly:MMMM yyyy} has {daysInMonth} days in the Hijri Calendar");
