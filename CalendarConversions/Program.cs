b129a0553fb62d416fd999df87b7e15dd6144a42
Conrad Akunga <conradakunga@gmail.com>
2025-10-01 22:00:25 +0300

    Init commit

---
+++ CalendarConversions/Program.cs
@@ -0,0 +1,10 @@
+﻿//Create the date in the Julian Calendar	
+
+using NodaTime;
+
+var julianDate = new LocalDate(1582, 10, 5, CalendarSystem.Julian);
+Console.WriteLine($"Julian: {julianDate}");
+
+//Convert the date to the Gregorian calendar
+var gregorianDate = julianDate.WithCalendar(CalendarSystem.Gregorian);
+Console.WriteLine($"Gregorian: {gregorianDate}");
