985909a67060e0bec505b396f9df0e01d8005266
Conrad Akunga <conradakunga@gmail.com>
2025-10-10 21:14:59 +0300

    Init commit

---
+++ WriteToEventLog/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+const string applicationName = "My Awesome App";
+const string logName = "Application";
+
+// Check if source exists
+if (!EventLog.Exists(applicationName))
+{
+    // Create source
+    EventLog.CreateEventSource(applicationName, logName);
+}
+
+// Create a logger
+using (var logger = new EventLog(logName))
+{
+    // Set the source
+    logger.Source = applicationName;
+    // Write the entry
+    logger.WriteEntry("This is a test", EventLogEntryType.Information);
+}
