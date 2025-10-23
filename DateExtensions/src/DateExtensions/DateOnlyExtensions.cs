ef37aa18ca9273dca9ff3085858f591a8c4970e8
Conrad Akunga <conradakunga@gmail.com>
2025-10-23 21:58:43 +0300

    Init commit

---
+++ DateExtensions/src/DateExtensions/DateOnlyExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace DateExtensions;
+
+public static class DateOnlyExtensions
+{
+    /// <summary>
+    /// Get the current quarter of the date
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
+    public static int Quarter(this DateOnly date)
+    {
+        return (date.Month - 1) / 3 + 1;
+    }
+
+    /// <summary>
+    /// Returns the start date of the quarter for the given date.
+    /// </summary>
+    public static DateOnly GetStartOfQuarter(this DateOnly date)
+    {
+        int startMonth = (date.Quarter() - 1) * 3 + 1;
+        return new DateOnly(date.Year, startMonth, 1);
+    }
+
+    /// <summary>
+    /// Returns the end date of the quarter for the given date.
+    /// </summary>
+    public static DateOnly GetEndOfQuarter(this DateOnly date)
+    {
+        int endMonth = date.Quarter() * 3;
+        int endDay = DateTime.DaysInMonth(date.Year, endMonth);
+        return new DateOnly(date.Year, endMonth, endDay);
+    }
+}
---
+++ DateExtensions/tests/DateExtensionTests/DateExtensionTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using AwesomeAssertions;
+using DateExtensions;
+
+namespace DateExtensionTests;
+
+public class DateExtensionTests
+{
+    [Theory]
+    [InlineData(1, 1, 1)]
+    [InlineData(2, 1, 1)]
+    [InlineData(3, 1, 1)]
+    [InlineData(4, 1, 2)]
+    [InlineData(5, 1, 2)]
+    [InlineData(6, 1, 2)]
+    [InlineData(7, 1, 3)]
+    [InlineData(8, 1, 3)]
+    [InlineData(9, 1, 3)]
+    [InlineData(10, 1, 4)]
+    [InlineData(11, 1, 4)]
+    [InlineData(12, 1, 4)]
+    public void QuarterTests(int month, int day, byte quarter)
+    {
+        var sut = new DateOnly(2025, month, day);
+        sut.Quarter().Should().Be(quarter);
+    }
+
+    [Theory]
+    [InlineData(1, 1, 1, 1)]
+    [InlineData(3, 1, 1, 1)]
+    [InlineData(4, 1, 4, 1)]
+    [InlineData(6, 1, 4, 1)]
+    [InlineData(7, 1, 7, 1)]
+    [InlineData(9, 1, 7, 1)]
+    [InlineData(10, 1, 10, 1)]
+    [InlineData(12, 1, 10, 1)]
+    public void StartOfQuarterTests(int testMonth, int testDay, int actualMonth, int actualDay)
+    {
+        var testDate = new DateOnly(2025, testMonth, testDay);
+        testDate.GetStartOfQuarter().Should().Be(new DateOnly(2025, actualMonth, actualDay));
+    }
+
+    [Theory]
+    [InlineData(1, 1, 3, 31)]
+    [InlineData(3, 31, 3, 31)]
+    [InlineData(4, 1, 6, 30)]
+    [InlineData(6, 30, 6, 30)]
+    [InlineData(7, 1, 9, 30)]
+    [InlineData(9, 30, 9, 30)]
+    [InlineData(10, 1, 12, 31)]
+    [InlineData(12, 31, 12, 31)]
+    public void EndOfQuarterTests(int testMonth, int testDay, int actualMonth, int actualDay)
+    {
+        var testDate = new DateOnly(2025, testMonth, testDay);
+        testDate.GetEndOfQuarter().Should().Be(new DateOnly(2025, actualMonth, actualDay));
+    }
+}
