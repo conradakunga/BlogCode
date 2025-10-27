ea575e0f2e6084f71d34b71588920f6a7786ab52
Conrad Akunga <conradakunga@gmail.com>
2025-10-27 10:39:24 +0300

    Init commit

---
+++ 2025-10-26 - DateExtensions/src/DateExtensions/DateOnlyExtensions.cs
@@ -0,0 +1,159 @@
+﻿namespace DateExtensions;
+
+public static class DateOnlyExtensions
+{
+    /// <summary>
+    /// Get the current quarter of the date
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
+    public static int Quarter(this DateOnly date)
+    {
+        return (date.Month - 1) / 3 + 1;
+    }
+
+    /// <summary>
+    /// Returns the start date of the quarter for the given date.
+    /// </summary>
+    public static DateOnly GetStartOfQuarter(this DateOnly date)
+    {
+        int startMonth = (date.Quarter() - 1) * 3 + 1;
+        return new DateOnly(date.Year, startMonth, 1);
+    }
+
+    /// <summary>
+    /// Returns the end date of the quarter for the given date.
+    /// </summary>
+    public static DateOnly GetEndOfQuarter(this DateOnly date)
+    {
+        int endMonth = date.Quarter() * 3;
+        int endDay = DateTime.DaysInMonth(date.Year, endMonth);
+        return new DateOnly(date.Year, endMonth, endDay);
+    }
+
+    /// <summary>
+    /// Returns the last day of the previous quarter
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
+    public static DateOnly GetEndOfPreviousQuarter(this DateOnly date)
+    {
+        // Get the start of the current quarter
+        var startOfCurrentQuarter = date.GetStartOfQuarter();
+        // Subtract one day
+        return startOfCurrentQuarter.AddDays(-1);
+    }
+
+    /// <summary>
+    /// Returns the first day of the previous quarter
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
+    public static DateOnly GetStartOfPreviousQuarter(this DateOnly date)
+    {
+        // Get the start of the current quarter
+        var startOfCurrentQuarter = date.GetStartOfQuarter();
+        // Subtract 3 months
+        return startOfCurrentQuarter.AddMonths(-3);
+    }
+
+    /// <summary>
+    /// Returns the first day of the next quarter
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
+    public static DateOnly GetStartOfNextQuarter(this DateOnly date)
+    {
+        // Get the end of the current quarter
+        var endOfCurrentQuarter = date.GetEndOfQuarter();
+        // Add a day
+        return endOfCurrentQuarter.AddDays(1);
+    }
+
+    /// <summary>
+    /// Returns the last day of the next quarter
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
+    public static DateOnly GetEndOfNextQuarter(this DateOnly date)
+    {
+        // Get the start of the next quarter
+        var startOfNextQuarter = date.GetStartOfNextQuarter();
+        // Get the end of the (now) current quarter
+        return startOfNextQuarter.GetEndOfQuarter();
+    }
+
+    /// <summary>
+    /// Returns the start of the current year
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
+    public static DateOnly GetStartOfCurrentYear(this DateOnly date)
+    {
+        // Create a new dateonly using the current year, first day, and first month (Jan)
+        return new DateOnly(date.Year, 1, 1);
+    }
+
+    /// <summary>
+    /// Returns the end of the current year
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
+    public static DateOnly GetEndOfCurrentYear(this DateOnly date)
+    {
+        // Get the start of the current year
+        var startOfCurrentYear = date.GetStartOfCurrentYear();
+        // Push to the next year
+        var startOfNextYear = new DateOnly(startOfCurrentYear.Year + 1, 1, 1);
+        // Advance to end of current year
+        return startOfNextYear.AddDays(-1);
+    }
+
+    /// <summary>
+    /// Returns the start of the previous year
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
+    public static DateOnly GetStartOfPreviousYear(this DateOnly date)
+    {
+        // Create a new dateonly using the current year minus one, first day, and first month (Jan)
+        return new DateOnly(date.Year - 1, 1, 1);
+    }
+
+    /// <summary>
+    /// Returns the end of the previous year
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
+    public static DateOnly GetEndOfPreviousYear(this DateOnly date)
+    {
+        // Get the start of the previous year
+        var startOfPreviousYear = date.GetStartOfPreviousYear();
+        // Return the end of that year
+        return startOfPreviousYear.GetEndOfCurrentYear();
+    }
+
+    /// <summary>
+    /// Returns the start of the next year
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
+    public static DateOnly GetStartOfNextYear(this DateOnly date)
+    {
+        // Create a new dateonly using the current year plus one, first day, and first month (Jan)
+        return new DateOnly(date.Year + 1, 1, 1);
+    }
+
+    /// <summary>
+    /// Return the end of the next year
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
+    public static DateOnly GetEndOfNextYear(this DateOnly date)
+    {
+        // Get the start of the next year
+        var startOfNextYear = date.GetStartOfNextYear();
+        // Return the end of that year
+        return startOfNextYear.GetEndOfCurrentYear();
+    }
+}
---
+++ 2025-10-26 - DateExtensions/tests/DateExtensionTests/DateExtensionTests.cs
@@ -0,0 +1,169 @@
+﻿using AwesomeAssertions;
+using DateExtensions;
+
+namespace DateExtensionTests;
+
+public class DateExtensionTests
+{
+    [Trait("Quarter", "General")]
+    [Theory]
+    [InlineData(1, 1, 1)]
+    [InlineData(2, 1, 1)]
+    [InlineData(3, 1, 1)]
+    [InlineData(4, 1, 2)]
+    [InlineData(5, 1, 2)]
+    [InlineData(6, 1, 2)]
+    [InlineData(7, 1, 3)]
+    [InlineData(8, 1, 3)]
+    [InlineData(9, 1, 3)]
+    [InlineData(10, 1, 4)]
+    [InlineData(11, 1, 4)]
+    [InlineData(12, 1, 4)]
+    public void QuarterTests(int month, int day, byte quarter)
+    {
+        var sut = new DateOnly(2025, month, day);
+        sut.Quarter().Should().Be(quarter);
+    }
+
+    [Trait("Quarter", "Start")]
+    [Theory]
+    [InlineData(1, 1, 1, 1)]
+    [InlineData(3, 1, 1, 1)]
+    [InlineData(4, 1, 4, 1)]
+    [InlineData(6, 1, 4, 1)]
+    [InlineData(7, 1, 7, 1)]
+    [InlineData(9, 1, 7, 1)]
+    [InlineData(10, 1, 10, 1)]
+    [InlineData(12, 1, 10, 1)]
+    public void StartOfQuarterTests(int testMonth, int testDay, int actualMonth, int actualDay)
+    {
+        var testDate = new DateOnly(2025, testMonth, testDay);
+        testDate.GetStartOfQuarter().Should().Be(new DateOnly(2025, actualMonth, actualDay));
+    }
+
+    [Trait("Quarter", "End")]
+    [Theory]
+    [InlineData(1, 1, 3, 31)]
+    [InlineData(3, 31, 3, 31)]
+    [InlineData(4, 1, 6, 30)]
+    [InlineData(6, 30, 6, 30)]
+    [InlineData(7, 1, 9, 30)]
+    [InlineData(9, 30, 9, 30)]
+    [InlineData(10, 1, 12, 31)]
+    [InlineData(12, 31, 12, 31)]
+    public void EndOfQuarterTests(int testMonth, int testDay, int actualMonth, int actualDay)
+    {
+        var testDate = new DateOnly(2025, testMonth, testDay);
+        testDate.GetEndOfQuarter().Should().Be(new DateOnly(2025, actualMonth, actualDay));
+    }
+
+    [Trait("Quarter", "End")]
+    [Theory]
+    [InlineData(2025, 4, 2, 2025, 3, 31)]
+    [InlineData(2025, 1, 1, 2024, 12, 31)]
+    public void EndOfPreviousQuarterTests(int testYear, int testMonth, int testDay, int actualYear, int actualMonth,
+        int actualDay)
+    {
+        var testDate = new DateOnly(testYear, testMonth, testDay);
+        testDate.GetEndOfPreviousQuarter().Should().Be(new DateOnly(actualYear, actualMonth, actualDay));
+    }
+
+    [Trait("Quarter", "Start")]
+    [Theory]
+    [InlineData(2025, 4, 2, 2025, 1, 1)]
+    [InlineData(2025, 1, 2, 2024, 10, 1)]
+    public void StartOfPreviousQuarterTests(int testYear, int testMonth, int testDay, int actualYear, int actualMonth,
+        int actualDay)
+    {
+        var testDate = new DateOnly(testYear, testMonth, testDay);
+        testDate.GetStartOfPreviousQuarter().Should().Be(new DateOnly(actualYear, actualMonth, actualDay));
+    }
+
+    [Trait("Quarter", "Start")]
+    [Theory]
+    [InlineData(2025, 4, 2, 2025, 7, 1)]
+    [InlineData(2025, 12, 31, 2026, 1, 1)]
+    public void StartOfNextQuarterTests(int testYear, int testMonth, int testDay, int actualYear, int actualMonth,
+        int actualDay)
+    {
+        var testDate = new DateOnly(testYear, testMonth, testDay);
+        testDate.GetStartOfNextQuarter().Should().Be(new DateOnly(actualYear, actualMonth, actualDay));
+    }
+
+    [Trait("Quarter", "End")]
+    [Theory]
+    [InlineData(2025, 4, 2, 2025, 9, 30)]
+    [InlineData(2025, 12, 31, 2026, 3, 31)]
+    public void EndOfNextQuarterTests(int testYear, int testMonth, int testDay, int actualYear, int actualMonth,
+        int actualDay)
+    {
+        var testDate = new DateOnly(testYear, testMonth, testDay);
+        testDate.GetEndOfNextQuarter().Should().Be(new DateOnly(actualYear, actualMonth, actualDay));
+    }
+
+    [Trait("Year", "Start")]
+    [Theory]
+    [InlineData(2025, 4, 2, 2025, 1, 1)]
+    [InlineData(2025, 12, 31, 2025, 1, 1)]
+    public void StartOfCurrentYearTests(int testYear, int testMonth, int testDay, int actualYear, int actualMonth,
+        int actualDay)
+    {
+        var testDate = new DateOnly(testYear, testMonth, testDay);
+        testDate.GetStartOfCurrentYear().Should().Be(new DateOnly(actualYear, actualMonth, actualDay));
+    }
+
+    [Trait("Year", "End")]
+    [Theory]
+    [InlineData(2025, 4, 2, 2025, 12, 31)]
+    [InlineData(2025, 12, 31, 2025, 12, 31)]
+    public void EndOfCurrentYearTests(int testYear, int testMonth, int testDay, int actualYear, int actualMonth,
+        int actualDay)
+    {
+        var testDate = new DateOnly(testYear, testMonth, testDay);
+        testDate.GetEndOfCurrentYear().Should().Be(new DateOnly(actualYear, actualMonth, actualDay));
+    }
+
+    [Trait("Year", "Start")]
+    [Theory]
+    [InlineData(2025, 4, 2, 2024, 1, 1)]
+    [InlineData(2025, 12, 31, 2024, 1, 1)]
+    public void StartOfPreviousYearTests(int testYear, int testMonth, int testDay, int actualYear, int actualMonth,
+        int actualDay)
+    {
+        var testDate = new DateOnly(testYear, testMonth, testDay);
+        testDate.GetStartOfPreviousYear().Should().Be(new DateOnly(actualYear, actualMonth, actualDay));
+    }
+
+    [Trait("Year", "End")]
+    [Theory]
+    [InlineData(2025, 4, 2, 2024, 12, 31)]
+    [InlineData(2025, 12, 31, 2024, 12, 31)]
+    public void EndOfPreviousYearTests(int testYear, int testMonth, int testDay, int actualYear, int actualMonth,
+        int actualDay)
+    {
+        var testDate = new DateOnly(testYear, testMonth, testDay);
+        testDate.GetEndOfPreviousYear().Should().Be(new DateOnly(actualYear, actualMonth, actualDay));
+    }
+
+    [Trait("Year", "Start")]
+    [Theory]
+    [InlineData(2025, 4, 2, 2026, 1, 1)]
+    [InlineData(2025, 12, 31, 2026, 1, 1)]
+    public void StartOfNextYearTests(int testYear, int testMonth, int testDay, int actualYear, int actualMonth,
+        int actualDay)
+    {
+        var testDate = new DateOnly(testYear, testMonth, testDay);
+        testDate.GetStartOfNextYear().Should().Be(new DateOnly(actualYear, actualMonth, actualDay));
+    }
+
+    [Trait("Year", "End")]
+    [Theory]
+    [InlineData(2025, 4, 2, 2026, 12, 31)]
+    [InlineData(2025, 12, 31, 2026, 12, 31)]
+    public void EndOfNextYearTests(int testYear, int testMonth, int testDay, int actualYear, int actualMonth,
+        int actualDay)
+    {
+        var testDate = new DateOnly(testYear, testMonth, testDay);
+        testDate.GetEndOfNextYear().Should().Be(new DateOnly(actualYear, actualMonth, actualDay));
+    }
+}
