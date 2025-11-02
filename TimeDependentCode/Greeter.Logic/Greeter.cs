a9b5e529ac3f8da51d04dce7f22c6426735271f1
Conrad Akunga <conradakunga@gmail.com>
2025-11-02 15:32:51 +0300

    Init commit

---
+++ TimeDependentCode/Greeter.Logic/Greeter.cs
@@ -0,0 +1,22 @@
+﻿namespace GreeterLogic;
+
+public sealed class Greeter
+{
+    private readonly TimeProvider _timeProvider;
+
+    public Greeter(TimeProvider timeProvider)
+    {
+        _timeProvider = timeProvider;
+    }
+
+    public string Greet()
+    {
+        return _timeProvider.GetLocalNow().Hour switch
+        {
+            >= 0 and < 13 => "Good Morning",
+            >= 13 and <= 16 => "Good Afternoon",
+            > 16 and <= 20 => "Good Evening",
+            _ => "Good Night"
+        };
+    }
+}
---
+++ TimeDependentCode/Greeter.Tests/GreeterTests.cs
@@ -0,0 +1,29 @@
+﻿using AwesomeAssertions;
+using GreeterLogic;
+using Microsoft.Extensions.Time.Testing;
+
+namespace GreeterTests;
+
+public class GreeterTests
+{
+    [Theory]
+    [InlineData(1, "Good Morning")]
+    [InlineData(12, "Good Morning")]
+    [InlineData(13, "Good Afternoon")]
+    [InlineData(16, "Good Afternoon")]
+    [InlineData(17, "Good Evening")]
+    [InlineData(20, "Good Evening")]
+    [InlineData(21, "Good Night")]
+    [InlineData(23, "Good Night")]
+    public void Greeter_Returns_Correct_Greeting(int hour, string greeting)
+    {
+        // Create a fake time provider
+        var provider = new FakeTimeProvider();
+        // Set the time based on passed values (hour)
+        provider.SetUtcNow(new DateTime(2024, 1, 1, hour, 1, 0));
+        // Create instance of system under test
+        var sut = new Greeter(provider);
+        // Assert result
+        sut.Greet().Should().Be(greeting);
+    }
+}
