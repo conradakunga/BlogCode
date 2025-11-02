57f48773fdf5d1163c81acf89439bacd869786d1
Conrad Akunga <conradakunga@gmail.com>
2025-11-02 15:34:17 +0300

    Rename folders

---
+++ 2025-11-01 - DateExtensions/src/DateExtensions/DateOnlyExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace DateExtensions;
+
+public static class DateOnlyExtensions
+{
+    extension(DateOnly)
+    {
+        /// <summary>
+        /// Create a new DateOnly offset by selected number of years
+        /// </summary>
+        /// <returns></returns>
+        public static DateOnly CreateCurrentWithOffsetYear(int years)
+        {
+            var now = DateOnly.FromDateTime(DateTime.Now);
+            return new DateOnly(now.Year + years, now.Month, now.Day);
+        }
+    }
+}
---
+++ 2025-11-01 - DateExtensions/tests/DateExtensionTests/DateExtensionTests.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using AwesomeAssertions;
+using DateExtensions;
+
+namespace DateExtensionTests;
+
+public class DateExtensionTests
+{
+    [Theory]
+    [InlineData(2024, 1, 2025)]
+    public void YearTests(int currentYear, int offsetYears, int expectedYear)
+    {
+        var provider = new FakeDateTimeProvider
+        var sut = new DateOnly(currentYear, 1, 1);
+        sut.Quarter().Should().Be(quarter);
+    }
+}
