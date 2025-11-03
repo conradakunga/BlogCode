00d4c57e50783e257afbc98bbda6534d253bcec4
Conrad Akunga <conradakunga@gmail.com>
2025-11-03 13:04:43 +0300

    Init commit

---
+++ 2025-11-02 - DateExtensions/src/DateExtensions/DateOnlyExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using Humanizer;
+
+namespace DateExtensions;
+
+public static class DateOnlyExtensions
+{
+    extension(DateOnly)
+    {
+        /// <summary>
+        /// Return the calendar name for the current culture
+        /// </summary>
+        /// <returns></returns>
+        public static string CalendarName
+        {
+            get
+            {
+                var calendar = CultureInfo.CurrentCulture.Calendar;
+                return calendar.GetType().Name.Humanize();
+            }
+        }
+    }
+}
---
+++ 2025-11-02 - DateExtensions/tests/DateExtensionTests/DateExtensionTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using AwesomeAssertions;
+using DateExtensions;
+
+namespace DateExtensionTests;
+
+public class DateExtensionTests
+{
+    [Fact]
+    public void CultureName_Is_Returned_Correctly()
+    {
+        CultureInfo.CurrentCulture = new CultureInfo("en-KE");
+        DateOnly.CalendarName.Should().Be("Gregorian calendar");
+    }
+}
