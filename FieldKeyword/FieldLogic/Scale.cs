40a53891ad68701fbe79398de7c4a7743e5b3fc5
Conrad Akunga <conradakunga@gmail.com>
2025-11-05 18:09:24 +0300

    Init commit

---
+++ FieldKeyword/FieldLogic/Scale.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.Contracts;
+
+
+namespace FieldLogic.V1
+{
+    public sealed class Scale
+    {
+        public required int Temperature { get; init; }
+    }
+}
+
+namespace FieldLogic.V2
+{
+    public sealed class Scale
+    {
+        private int _temperature;
+
+        public int Temperature
+        {
+            get => _temperature;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException(nameof(value),
+                        $"{value} is an invalid temperature. Must be greater than 0");
+                _temperature = value;
+            }
+        }
+    }
+}
+
+namespace FieldLogic.V3
+{
+    public sealed class Scale
+    {
+        public int Temperature
+        {
+            get;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException(nameof(value),
+                        $"{value} is an invalid temperature. Must be greater than 0");
+                field = value;
+            }
+        }
+    }
+}
---
+++ FieldKeyword/FieldLogicTests/ScaleTests.cs
@@ -0,0 +1,35 @@
+﻿using AwesomeAssertions;
+using FieldLogic.V3;
+
+namespace FieldLogicTests;
+
+public class ScaleTests
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(0)]
+    [InlineData(100)]
+    public void Scale_Initializes_Correctly(int temperature)
+    {
+        var scale = new Scale()
+        {
+            Temperature = temperature
+        };
+        scale.Temperature.Should().Be(temperature);
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(-10)]
+    public void Scale_Throws_Exception_With_Invalid_Value(int temperature)
+    {
+        var ex = Record.Exception(() =>
+        {
+            var scale = new Scale()
+            {
+                Temperature = temperature
+            };
+        });
+        ex.Should().BeOfType<ArgumentOutOfRangeException>();
+    }
+}
