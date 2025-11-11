f858799bc30a709d91af3b338ef6ad345ce44b9d
Conrad Akunga <conradakunga@gmail.com>
2025-11-11 20:20:41 +0300

    Init commit

---
+++ SplittingOfStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿var input = "This is a string that we want to split";
+var splitArray = input.Split(" ");
+foreach (var entry in splitArray)
+{
+    Console.WriteLine($"'{entry}'");
+}
+
+// This string has spaces
+input = "This is a    string that   we want to   split";
+splitArray = input.Split(" ");
+foreach (var entry in splitArray)
+{
+    Console.WriteLine($"'{entry}'");
+}
+
+// Split the string with spaces and remove empty elements
+splitArray = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+foreach (var entry in splitArray)
+{
+    Console.WriteLine($"'{entry}'");
+}
