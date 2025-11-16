0cec9521e723977e8b595454f6ec90b44d8f6e13
Conrad Akunga <conradakunga@gmail.com>
2025-11-17 01:13:27 +0300

    Init commit

---
+++ ReadNumberFromString/Person.cs
@@ -0,0 +1,5 @@
+public sealed class Person
+{
+    public string Name { get; set; }
+    public byte Age { get; set; }
+}
---
+++ ReadNumberFromString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+
+var person = new Person { Name = "James Bond", Age = 40 };
+// Configure options for serialization
+var options = new JsonSerializerOptions
+{
+    WriteIndented = true
+};
+// Serialize
+var json = JsonSerializer.Serialize(person, options);
+// Print to console
+Console.WriteLine(json);
+
+var apiJson =
+    """
+    {
+      "Name": "James Bond",
+      "Age": "40"
+    }
+    """;
+// Configure options for deserialization
+var serializationOptions = new JsonSerializerOptions
+{
+    NumberHandling = System.Text.Json.Serialization.JsonNumberHandling.AllowReadingFromString
+};
+// Deserialize to an object
+person = JsonSerializer.Deserialize<Person>(apiJson, serializationOptions)!;
+// Print details
+Console.WriteLine($"{person.Name} is {person.Age} years old");
