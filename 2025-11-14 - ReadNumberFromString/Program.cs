4effd46c7a0bcbefa9c2500f38a7d91600d5d661
Conrad Akunga <conradakunga@gmail.com>
2025-11-17 01:25:59 +0300

    Init commit

---
+++ 2025-11-14 - ReadNumberFromString/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+var person = new Person { Name = "James Bond", Age = 40 };
+// Configure options for serialization
+var options = new JsonSerializerOptions
+{
+    WriteIndented = true,
+    NumberHandling = System.Text.Json.Serialization.JsonNumberHandling.WriteAsString
+};
+// Serialize
+var json = JsonSerializer.Serialize(person, options);
+// Print to console
+Console.WriteLine(json);
