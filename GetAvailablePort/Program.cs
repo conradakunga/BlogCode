e7060ba0158bcf91382ddc021f118de53501986d
Conrad Akunga <conradakunga@gmail.com>
2025-11-18 21:56:42 +0300

    Init commit

---
+++ GetAvailablePort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+Console.WriteLine($"TCPListener free port: {TcpListenerGetFreePort()}");
+Console.WriteLine($"Socket free port: {SocketGetFreePort()}");
+return;
+
+int TcpListenerGetFreePort()
+{
+    using var listener = new TcpListener(IPAddress.Loopback, 0);
+    listener.Start();
+    return ((IPEndPoint)listener.LocalEndpoint).Port;
+}
+
+int SocketGetFreePort()
+{
+    using (var s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+    {
+        s.Bind(new IPEndPoint(IPAddress.Loopback, 0));
+        return ((IPEndPoint)s.LocalEndPoint!).Port;
+    }
+}
