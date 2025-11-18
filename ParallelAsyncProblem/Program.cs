7e0bdeeeec31eef39290369f94ca448c60619f8c
Conrad Akunga <conradakunga@gmail.com>
2025-11-18 10:24:15 +0300

    Init commit

---
+++ ParallelAsyncProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+string[] collection = ["One", "Two", "Three"];
+
+Parallel.ForEach(collection, async (element) =>
+{
+    // Create database connection
+    await using (var cn = new SqlConnection("......"))
+    {
+        // Execute query asynchronously
+        await cn.ExecuteAsync("Query", new { ID = element });
+    }
+});
+
