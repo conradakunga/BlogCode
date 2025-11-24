b15ec583791f202e18a0c97f890f0d515b941e48
Conrad Akunga <conradakunga@gmail.com>
2025-11-24 23:14:54 +0300

    Init commit

---
+++ 2025-11-20 - AggregatedExceptionHandling/Program.cs
@@ -0,0 +1,18 @@
+﻿try
+{
+    Parallel.For(0, 3, i =>
+    {
+        // Throw a known exception
+        throw new InvalidOperationException("Some invalid thing happened");
+    });
+}
+catch (AggregateException ex)
+{
+    Console.WriteLine("There were {0} exceptions ", ex.InnerExceptions.Count);
+
+    var counter = 0;
+    foreach (var e in ex.InnerExceptions)
+    {
+        Console.WriteLine($"Error {++counter}: {e.Message}");
+    }
+}
