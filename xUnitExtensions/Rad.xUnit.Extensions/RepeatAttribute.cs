16197c903569b309f00215253004daf171b6ed23
Conrad Akunga <conradakunga@gmail.com>
2025-11-25 19:37:10 +0300

    Init commit

---
+++ xUnitExtensions/Rad.xUnit.Extensions/RepeatAttribute.cs
@@ -0,0 +1,41 @@
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace Rad.xUnit.Extensions;
+
+/// <summary>
+/// Runs tests multiple times
+/// </summary>
+public sealed class RepeatAttribute : DataAttribute
+{
+    private readonly int _count;
+
+    /// <summary>
+    /// Number of times to run the tests
+    /// </summary>
+    /// <param name="count"></param>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public RepeatAttribute(int count)
+    {
+        if (count < 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(count), "Repeat count must be greater than 0."
+            );
+        }
+
+        _count = count;
+    }
+
+    /// <summary>
+    /// Returns an enumerable to be used by the test runner
+    /// </summary>
+    /// <param name="_"></param>
+    /// <returns></returns>
+    public override IEnumerable<object[]> GetData(MethodInfo _)
+    {
+        foreach (var iteration in Enumerable.Range(0, _count))
+        {
+            yield return [iteration];
+        }
+    }
+}
---
+++ xUnitExtensions/Rad.xUnit.Extensions/WindowsOnlyFactAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Rad.xUnit.Extensions;
+
+/// <summary>
+/// Marks a test as only runnable in Windows
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class WindowsOnlyFactAttribute : FactAttribute
+{
+    public WindowsOnlyFactAttribute()
+    {
+        if (!OperatingSystem.IsWindows())
+        {
+            Skip = "This is a Windows Only Test";
+        }
+    }
+}
