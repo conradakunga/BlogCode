70204bf9aff13fee7d3f069bc4b0e199d8cd47ad
Conrad Akunga <conradakunga@gmail.com>
2025-11-25 17:03:38 +0300

    Init commit

---
+++ 2025-11-22 - NestedExceptions/Program.cs
@@ -0,0 +1,15 @@
+﻿var task = Task.WhenAll(
+    Task.Run(() => throw new Exception("Outer error")),
+    Task.Run(() => throw new AggregateException(new InvalidOperationException("Inner Level 1"),
+        new ArgumentException("Inner Level 2")
+    )));
+try
+{
+    task.Wait();
+}
+catch (AggregateException ex)
+{
+    var flattened = ex.Flatten();
+    foreach (var innerException in flattened.InnerExceptions)
+        Console.WriteLine(innerException.Message);
+}
