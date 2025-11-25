f3297902cbf7d06aa3c225c25868e10b0dcdf9c2
Conrad Akunga <conradakunga@gmail.com>
2025-11-25 17:40:37 +0300

    Minor refactor

--- 2025-11-23 - TryParse/Program.cs
+++ 2025-11-23 - TryParse/Program.cs
@@ -4,15 +4,15 @@ string[] cultures = ["en-US", "en-GB", "fr-FR", "es-ES"];
 foreach (var culture in cultures)
 {
     Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
-    const string ParseFormat = "d/M/yyyy";
+    const string parseFormat = "d/M/yyyy";
     var dateString = "2/1/2025";
-    if (DateTime.TryParseExact(dateString, ParseFormat, CultureInfo.InvariantCulture, DateTimeStyles.None,
+    if (DateTime.TryParseExact(dateString, parseFormat, CultureInfo.InvariantCulture, DateTimeStyles.None,
             out DateTime date))
     {
         Console.WriteLine($"{culture} - {date:d MMM yyyy}");
     }
     else
     {
-        Console.WriteLine($"Could not parse {dateString} in the format {ParseFormat}");
+        Console.WriteLine($"Could not parse {dateString} in the format {parseFormat}");
     }
 }
