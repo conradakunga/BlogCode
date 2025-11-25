3d13e288ac4e4ca76266baf9c62dbd669f96ac83
Conrad Akunga <conradakunga@gmail.com>
2025-11-25 16:10:10 +0300

    Init commit

---
+++ NestedExceptions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+try
+{
+    throw new Exception("This is the level 1",
+        new Exception("This is the level 2", new Exception("This is the level 3")));
+}
+catch (Exception ex)
+{
+    // Console.WriteLine(ex.Message);
+    StringBuilder sb = new();
+    Console.WriteLine(GetMessage(ex, sb));
+}
+
+try
+{
+    throw new Exception("This is the level 1",
+        new Exception("This is the level 2", new Exception("This is the level 3")));
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+return;
+
+string GetMessage(Exception ex, StringBuilder sb)
+{
+    sb.AppendLine(ex.Message);
+    if (ex.InnerException is not null)
+    {
+        GetMessage(ex.InnerException, sb);
+    }
+
+    return sb.ToString();
+}
