fb4e03a7769ac69557922077e953a1bf98aa81d7
Conrad Akunga <conradakunga@gmail.com>
2025-11-25 17:39:46 +0300

    Init commit

---
+++ 2025-11-23 - TryParse/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+string[] cultures = ["en-US", "en-GB", "fr-FR", "es-ES"];
+foreach (var culture in cultures)
+{
+    Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
+    const string ParseFormat = "d/M/yyyy";
+    var dateString = "2/1/2025";
+    if (DateTime.TryParseExact(dateString, ParseFormat, CultureInfo.InvariantCulture, DateTimeStyles.None,
+            out DateTime date))
+    {
+        Console.WriteLine($"{culture} - {date:d MMM yyyy}");
+    }
+    else
+    {
+        Console.WriteLine($"Could not parse {dateString} in the format {ParseFormat}");
+    }
+}
