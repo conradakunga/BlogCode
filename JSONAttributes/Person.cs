8041fd6a568bd29ad7ac9d89b044ed86724bda1e
Conrad Akunga <conradakunga@gmail.com>
2025-12-12 12:51:23 +0300

    Init commit

---
+++ JSONAttributes/Person.cs
@@ -0,0 +1,7 @@
+namespace JSONAttributes;
+
+public sealed class Person
+{
+    public required string Firstname { get; init; }
+    public required string Surname { get; init; }
+}
---
+++ JSONAttributes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+using JSONAttributes;
+
+const string json =
+    """
+    {
+      "Firstname": "James",
+      "Surname": "Bond"
+    }
+    """;
+
+var person = JsonSerializer.Deserialize<Person>(json)!;
+Console.WriteLine($"Firstname: {person.Firstname}, Surname: {person.Surname}");
+
+const string jsonAttack =
+    """
+    {
+      "Firstname": "James",
+      "Surname": "Bond",
+      "Surname": "Bourne"
+    }
+    """;
+
+person = JsonSerializer.Deserialize<Person>(jsonAttack)!;
+Console.WriteLine($"Firstname: {person.Firstname}, Surname: {person.Surname}");
+
+
+var options = new JsonSerializerOptions
+{
+    AllowDuplicateProperties = false
+};
+
+try
+{
+    person = JsonSerializer.Deserialize<Person>(jsonAttack, options)!;
+    Console.WriteLine($"Firstname: {person.Firstname}, Surname: {person.Surname}");
+}
+catch (JsonException ex)
+{
+    Console.WriteLine(ex.Message);
+}
