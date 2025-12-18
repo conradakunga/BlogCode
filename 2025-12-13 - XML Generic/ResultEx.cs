d73121b37c3fabefb5684d028464abcdac4646e7
Conrad Akunga <conradakunga@gmail.com>
2025-12-18 09:53:18 +0300

    Innit commit

---
+++ 2025-12-13 - XML Generic/ResultEx.cs
@@ -0,0 +1,30 @@
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+public static class ResultEx
+{
+    public static async Task<IResult> Xml<T>(T value)
+    {
+        return Results.Text(await Serialize(value), "application/xml", Encoding.UTF8);
+    }
+
+    private static async Task<string> Serialize<T>(T value)
+    {
+        var serializer = new XmlSerializer(typeof(T));
+        await using var ms = new MemoryStream();
+        await using (var writer = XmlWriter.Create(
+                         ms,
+                         new XmlWriterSettings
+                         {
+                             Encoding = Encoding.UTF8,
+                             Indent = true,
+                             Async = true
+                         }))
+        {
+            serializer.Serialize(writer, value);
+        }
+
+        return Encoding.UTF8.GetString(ms.ToArray());
+    }
+}
