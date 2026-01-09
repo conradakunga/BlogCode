e311df8fa633e60710047790a061aa4b2b9f1dad
Conrad Akunga <conradakunga@gmail.com>
2026-01-09 18:08:48 +0300

    Update logic

---
+++ 2025-12-31 - Hangfire/HangfireTestBackground/Program.cs
@@ -0,0 +1,42 @@
+﻿using Hangfire;
+using Hangfire.AspNetCore;
+using Hangfire.PostgreSql;
+using Logic;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+// Configure logging
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+var services = new ServiceCollection();
+services.AddLogging(builder => builder.AddSerilog());
+// Register the job for DI
+services.AddTransient<NotificationJob>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+// Configure hangfire
+GlobalConfiguration.Configuration
+    .UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UsePostgreSqlStorage(x =>
+    {
+        x.UseNpgsqlConnection("host=localhost;Database=hangfire;username=myuser;password=mypassword");
+    })
+    // Configure how to resolve DI object activation
+    .UseActivator(new AspNetCoreJobActivator(serviceProvider.GetRequiredService<IServiceScopeFactory>()));
+
+// Start the server
+using (var server = new BackgroundJobServer())
+{
+    BackgroundJob.Enqueue<NotificationJob>(x => x.ExecuteLengthy());
+
+    // Artificial wait
+    await Task.Delay(TimeSpan.FromMinutes(5));
+
+    // Shutdown server
+    Log.Information("Shutting down BackgroundJobServer...");
+    await server.WaitForShutdownAsync(CancellationToken.None);
+}
--- 2025-12-31 - Hangfire/Logic/NotificationJob.cs
+++ 2025-12-31 - Hangfire/Logic/NotificationJob.cs
@@ -16,4 +16,11 @@ public sealed class NotificationJob
         _logger.LogInformation("Executing job ...");
         return Task.CompletedTask;
     }
+
+    public async Task ExecuteLengthy()
+    {
+        _logger.LogInformation("Executing long running job ...");
+        await Task.Delay(TimeSpan.FromMinutes(1));
+        _logger.LogInformation("Completed long running job ...");
+    }
 }
