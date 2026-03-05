7bf0034efc6893b4a98bfca0a3cf59b5b06ad3c5
Conrad Akunga <conradakunga@gmail.com>
2026-03-05 13:38:23 +0300

    Init commit

---
+++ 2026-01-07 - CompressZipFiles/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO.Compression;
+using System.Reflection;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console().CreateLogger();
+
+// Extract the current folder where the executable is running
+var currentFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+// Construct the full path to the source files
+var folderWithBooks = Path.Combine(currentFolder!, "Books");
+
+// Construct the full path to the zip file
+var targetZipFile = Path.Combine(folderWithBooks, "Books.zip");
+
+// Delete the zip file if it exists
+if (File.Exists(targetZipFile))
+    File.Delete(targetZipFile);
+
+// Retrieve the files
+var filesToZip = Directory.GetFiles(folderWithBooks);
+
+// Create the zip file on disk
+await using (var archive = ZipFile.Open(targetZipFile, ZipArchiveMode.Create))
+{
+    foreach (var file in filesToZip)
+    {
+        // Add each file to the zip file as an entry
+        await archive.CreateEntryFromFileAsync(file, Path.GetFileName(file), CompressionLevel.Optimal);
+    }
+}
+
+Log.Information("Written files in {SourceFiles} to {TargetZipFile}", folderWithBooks, targetZipFile);
