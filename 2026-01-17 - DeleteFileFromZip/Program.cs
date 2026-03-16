9be4f69bc1bd2f4aeab69006cad70df0fd0d1084
Conrad Akunga <conradakunga@gmail.com>
2026-03-17 01:09:12 +0300

    Init commit

---
+++ 2026-01-17 - DeleteFileFromZip/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console().CreateLogger();
+
+// Extract the current folder where the executable is running
+var currentFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+
+// Construct the full path to the zip file
+var targetZipFile = Path.Combine(currentFolder, "Books.zip");
+
+// Set the filename
+const string fileName = "wealth-of-nations.md";
+
+// Open the zip file on disk for update
+await using (var archive = ZipFile.Open(targetZipFile, ZipArchiveMode.Update))
+{
+    // If the entry exists, delete it 
+    var entry = archive.GetEntry(fileName);
+    if (entry is not null)
+    {
+        entry.Delete();
+        Log.Information("Deleted {File} from {Target}", fileName, targetZipFile);
+    }
+    else
+    {
+        Log.Warning("Could not file {File} in archive", fileName);
+    }
+}
