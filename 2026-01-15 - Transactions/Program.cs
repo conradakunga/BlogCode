b201966c25d53a7c8839211103ef76dfeaffe8e4
Conrad Akunga <conradakunga@gmail.com>
2026-03-16 23:52:40 +0300

    Fix logic bug

--- 2026-01-15 - Transactions/Program.cs
+++ 2026-01-15 - Transactions/Program.cs
@@ -6,7 +6,9 @@ Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
     .CreateLogger();
 
+//
 // Original
+//
 {
     await using (var cn = new SqlConnection(
                      "Data Source=.;uid=sa;pwd=YourStrongPassword123;TrustServerCertificate=true"))
@@ -31,27 +33,25 @@ Log.Logger = new LoggerConfiguration()
 // Multliple connections
 //
 {
-    {
-        var cn1 = new SqlConnection(
-            "Data Source=.;uid=sa;pwd=YourStrongPassword123;TrustServerCertificate=true");
-        await cn1.OpenAsync();
+    var cn1 = new SqlConnection(
+        "Data Source=.;uid=sa;pwd=YourStrongPassword123;TrustServerCertificate=true");
+    await cn1.OpenAsync();
 
-        var cn2 = new SqlConnection(
-            "Data Source=.;uid=sa;pwd=YourStrongPassword123;TrustServerCertificate=true");
-        await cn2.OpenAsync();
+    var cn2 = new SqlConnection(
+        "Data Source=.;uid=sa;pwd=YourStrongPassword123;TrustServerCertificate=true");
+    await cn2.OpenAsync();
 
-        var trans1 = await cn1.BeginTransactionAsync();
-        var trans2 = await cn2.BeginTransactionAsync();
-        // Create the processor
-        var processor = new TransactionProcessor();
-        // Do the first bit of work
-        var firstResult = await processor.DoThisThing(cn1, trans1);
-        // Do the next bit of work
-        var secondResult = await processor.DoTheOtherThing(cn2, trans2);
-        trans1.Commit();
-        trans2.Commit();
-        Log.Information("Success!");
-    }
+    var trans1 = await cn1.BeginTransactionAsync();
+    var trans2 = await cn2.BeginTransactionAsync();
+    // Create the processor
+    var processor = new TransactionProcessor();
+    // Do the first bit of work
+    var firstResult = await processor.DoThisThing(cn1, trans1);
+    // Do the next bit of work
+    var secondResult = await processor.DoTheOtherThing(cn2, trans2);
+    trans1.Commit();
+    trans2.Commit();
+    Log.Information("Success!");
 }
 //
 // Final refactor
