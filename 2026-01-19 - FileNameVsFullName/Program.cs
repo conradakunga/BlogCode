a999761014fb185ae26028580e2244761dac8b5d
Conrad Akunga <conradakunga@gmail.com>
2026-03-17 14:17:49 +0300

    Init commit

---
+++ 2026-01-19 - FileNameVsFullName/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO.Compression;
+using System.Reflection;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console().CreateLogger();
+
+// Extract the current folder where the executable is running
+var currentFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+
+// Construct the full path to the zip file
+var zilFile = Path.Combine(currentFolder, "Updated.zip");
+
+// Open the zip file on disk for update
+await using (var archive = await ZipFile.OpenAsync(zilFile, ZipArchiveMode.Read))
+{
+    // Loop through all the entries
+    for (var i = 0; i < archive.Entries.Count; i++)
+    {
+        // Get the file name
+        var file = archive.Entries[i];
+        // Print the count and name
+        Log.Information("File {Count} - Name: {Name}, FullName: {FullName}", i + 1, file.Name, file.FullName);
+    }
+}
