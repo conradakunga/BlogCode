58b3d109e7e347519748d4c5feda9c2f9dd94507
Conrad Akunga <conradakunga@gmail.com>
2026-03-20 20:27:07 +0300

    Init commit

---
+++ 2026-01-26 - 7zipCreatorPassword/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using CliWrap;
+using CliWrap.Buffered;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console().CreateLogger();
+
+// Extract the current folder where the executable is running
+var currentFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+
+// Construct the full path to the source files
+var folderWithBooks = Path.Combine(currentFolder, "Books");
+
+// Construct the full path to the zip file
+var target7ZipFile = Path.Combine(currentFolder, "Books.7z");
+
+// Path to 7zip executable
+const string executablePath = "/opt/homebrew/bin/7zz";
+
+// Archive password
+const string password = "A$tr0ngP@ssw0rD";
+
+// Delete 7zip file if it exists
+if (File.Exists(target7ZipFile))
+    File.Delete(target7ZipFile);
+
+var result = await Cli.Wrap(executablePath) // Set the path to the executable
+    .WithArguments(args => args
+            .Add("a") //Specify to create an archive
+            .Add("-t7z") // Specify the target format - 7z
+            .Add(target7ZipFile) // Taget file name
+            .Add($"{folderWithBooks}") // The files in the source folder
+            .Add($"-p{password}") // Set the password
+            .Add("-mhe=on") // encrypt file names
+            .Add("-mx=9") // max compression
+    )
+    .ExecuteBufferedAsync();
+
+// Check if the process succeeded
+if (result.ExitCode != 0)
+    Log.Error("7-Zip failed: {Message}", result.StandardError);
+else
+    Log.Information("Written files in {SourceFiles} to {Target7ZipFile} {Message}", folderWithBooks, target7ZipFile,
+        result.StandardOutput);
---
+++ 2026-01-27 - 7zipExtractor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Reflection;
+using CliWrap;
+using CliWrap.Buffered;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console().CreateLogger();
+
+// Extract the current folder where the executable is running
+var currentFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+
+// Set the folder we want our files extracted to
+var outputFolder = Path.Combine(currentFolder, "ExtractedBooks");
+
+// Construct the full path to the zip file
+var source7ZipFile = Path.Combine(currentFolder, "Books.7z");
+
+// Path to 7zip executable
+const string executablePath = "/opt/homebrew/bin/7zz";
+
+var result = await Cli.Wrap(executablePath) // Set the path to the executable
+    .WithArguments(args => args
+            .Add("x") //Specify to extract an archive
+            .Add(source7ZipFile) // Source zip file
+            .Add($"-o{outputFolder}") // The output folder
+            .Add("-y") // Say yes to all prompts
+    )
+    .ExecuteBufferedAsync();
+
+// Check if the process succeeded
+if (result.ExitCode != 0)
+    Log.Error("7-Zip failed: {Message}", result.StandardError);
+else
+    Log.Information("Extracted files in {SourceFiles} to {TargetFolder} {Message}", source7ZipFile, outputFolder,
+        result.StandardOutput);
