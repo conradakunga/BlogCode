1c745a7ecd3b45251cf66ee3adfc9617d5b63c4e
Conrad Akunga <conradakunga@gmail.com>
2026-03-21 12:43:32 +0300

    Listing Files In A 7-Zip Archive In C# & .NET

---
+++ 2026-01-29 - List7zFiles/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Reflection;
+using CliWrap;
+using CliWrap.Buffered;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console().CreateLogger();
+
+// Extract the current folder where the executable is running
+var currentFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+
+// Construct the full path to the zip file
+var source7ZipFile = Path.Combine(currentFolder, "Books.7z");
+
+// Path to 7zip executable
+const string executablePath = "/opt/homebrew/bin/7zz";
+
+var result = await Cli.Wrap(executablePath) // Set the path to the executable
+    .WithArguments(args => args
+            .Add("l") //Specify to list archive contents
+            .Add(source7ZipFile) // Source zip file
+    )
+    .ExecuteBufferedAsync();
+
+// Check if the process succeeded
+if (result.ExitCode != 0)
+    Log.Error("7-Zip failed: {Message}", result.StandardError);
+else
+    Log.Information("Files In {SourceZipFile} - {Listing}", source7ZipFile, result.StandardOutput);
