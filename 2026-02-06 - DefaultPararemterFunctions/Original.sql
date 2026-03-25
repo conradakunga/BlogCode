7b605ae5d6e3a0247ebb4687649b12503a63c05a
Conrad Akunga <conradakunga@gmail.com>
2026-03-25 21:59:25 +0300

    Passing Default Parameters to SQL Server Functions In C# & .NET

---
+++ 2026-02-06 - DefaultPararemterFunctions/Original.sql
@@ -0,0 +1,36 @@
+CREATE OR ALTER FUNCTION fn_GetDayOfWeek
+(
+    @Day TINYINT
+)
+    RETURNS NVARCHAR(15)
+AS
+BEGIN
+    DECLARE @Return NVARCHAR(15);
+    SELECT
+        @Return = CASE @Day
+                      WHEN 1
+                          THEN
+                          'Monday'
+                      WHEN 2
+                          THEN
+                          'Tuesday'
+                      WHEN 3
+                          THEN
+                          'Wednesday'
+                      WHEN 4
+                          THEN
+                          'Thursday'
+                      WHEN 5
+                          THEN
+                          'Friday'
+                      WHEN 6
+                          THEN
+                          'Satruday'
+                      WHEN 7
+                          THEN
+                          'Sunday'
+            END;
+    RETURN @Return;
+END;
+GO
+
---
+++ 2026-02-06 - DefaultPararemterFunctions/Program.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+const string connection = "Data Source=;database=Spies;uid=sa;pwd=YourStrongPassword123;TrustServerCertificate=True;";
+
+await using (var cn = new SqlConnection(connection))
+{
+    var result = await cn.QuerySingleAsync<string>("SELECT dbo.fn_GetDayOfWeek(@Day)", new { Day = 3 });
+    Console.WriteLine(result);
+}
+
+// await using (var cn = new SqlConnection(connection))
+// {
+//     var result = await cn.QuerySingleAsync<string>("SELECT dbo.fn_GetDayOfWeek(@Day)");
+//     Console.WriteLine(result);
+// }
+//
+await using (var cn = new SqlConnection(connection))
+{
+    var result = await cn.QuerySingleAsync<string>("SELECT dbo.fn_GetDayOfWeek(DEFAULT)");
+    Console.WriteLine(result);
+}
