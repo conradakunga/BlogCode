8e1b092effb8030ba0ea4b1c7e043a67f2fa5812
Conrad Akunga <conradakunga@gmail.com>
2026-04-14 14:32:46 +0300

    Deploying CLR Procedures & Functions In SQL Server 2022

---
+++ 2026-03-06 - Deploy SQLCLR/GenerateSqlServerAssembly.ps1
@@ -0,0 +1,34 @@
+﻿# Set variables
+$library = 'C:\Projects\innova-core\Innova.Database\bin\Release\Innova.Database.dll'
+$dllName = 'Innova.Database'
+$database = 'monkey'
+$server = '10.211.55.2'
+$username = 'sa'
+$password = 'YourStrongPassword123'
+
+# Get the MD5 hash of the build DLL
+$hash = (Get-FileHash -Algorithm SHA512 "C:\Projects\innova-core\Innova.Database\bin\Release\Innova.Database.dll").Hash
+
+# build the trusted assembly query
+$query = "EXEC sys.sp_add_trusted_assembly
+    @hash = 0x$hash,
+    @description = N'$dllName'"
+
+# Display the hash (in case you need to grab it
+Write-Host "The hash is - $hash"
+Invoke-Sqlcmd -ServerInstance $server -Database $database -Username $username -Password $password -Query $query -TrustServerCertificate
+
+# Get the assembly binary
+$bytes = [System.BitConverter]::ToString([System.IO.File]::ReadAllBytes($library)).Replace("-", "")
+
+# drop assembly if exists in the datavase
+# build the create assembly statement
+$query = "CREATE ASSEMBLY [$dllName]
+    FROM 0x$bytes"
+
+Write-Host "The bytes are $($bytes.Substring(0, 5))"
+
+Invoke-Sqlcmd -ServerInstance $server -Database $database -Username $username -Password $password -Query $query -TrustServerCertificate
+
+Write-Host 'CREATION COMPLETE'
+    
