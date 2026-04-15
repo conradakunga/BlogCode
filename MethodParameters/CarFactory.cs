2a193cae12f2d963195bb193860691834726467e
Conrad Akunga <conradakunga@gmail.com>
2026-04-15 12:11:02 +0300

    Code Housekeeping - Part 7 - Eschew Methods With Many Parameters

---
+++ MethodParameters/CarFactory.cs
@@ -0,0 +1,26 @@
+public class CarFactory
+{
+    public void CreateCar(string make, string model)
+    {
+    }
+
+    public void CreateCar(string make, string model, int year)
+    {
+    }
+
+    public void CreateCar(string make, string model, int year, string market)
+    {
+    }
+
+    public void CreateCar(string make, string model, int year, string market, bool leftHandDrive)
+    {
+    }
+
+    public void CreateCar(string make, string model, int year, string market, bool leftHandDrive, int engineCapacity)
+    {
+    }
+
+    public void CreateCar(CarCreateRequest request)
+    {
+    }
+}
---
+++ MethodParameters/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var factory = new CarFactory();
+factory.CreateCar("Subaru", "Outback", 2025, "Africa", false, 2000);
+
+var request = new CarCreateRequest("Subaru", "Outback", 2025, "Africa", false, 2000);
+factory.CreateCar(request);
+
+public sealed record CarCreateRequest(
+    string make,
+    string model,
+    int year,
+    string market,
+    bool leftHandDrive,
+    int engineCapacity);
