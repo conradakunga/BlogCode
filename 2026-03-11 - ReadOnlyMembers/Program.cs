8e2a7d8f23189eef484db7e741a592d4edbcfee8
Conrad Akunga <conradakunga@gmail.com>
2026-04-15 15:15:13 +0300

    2026-03-10

---
+++ 2026-03-11 - ReadOnlyMembers/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+public sealed class Car
+{
+    private readonly string _name;
+    private readonly string _make;
+    private readonly string _model;
+
+    public Car(string name, string make, string model)
+    {
+        _name = name;
+        _make = make;
+        _model = model;
+    }
+
+    public void Prepare()
+    {
+        _make = "Generic";
+    }
+}
