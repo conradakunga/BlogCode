3fd9adcb4afceb338e37e8ab33a1682e25d5a85a
Conrad Akunga <conradakunga@gmail.com>
2026-05-03 10:38:55 +0300

    Generating A Series Of Numbers In SQL Server

---
+++ 2026-03-26 - SQL Server Series/GenerateLinq/Program.cs
@@ -0,0 +1,11 @@
+﻿var numbers = Enumerable.Range(1, 10).ToList();
+numbers.ForEach(Console.WriteLine);
+
+var evenNumbers = Enumerable.Range(1, 10).Where(x => x % 2 == 0).ToList();
+evenNumbers.ForEach(Console.WriteLine);
+
+var oddNumbers = Enumerable.Range(1, 10).Where(x => x % 2 != 0).ToList();
+oddNumbers.ForEach(Console.WriteLine);
+
+var smallNumbers = Enumerable.Range(0, 20).Select(x => x / 20.0).ToList();
+smallNumbers.ForEach(Console.WriteLine);
