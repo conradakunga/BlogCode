ed2d56f476333d2265a6631612dc5b45845bea0a
Conrad Akunga <conradakunga@gmail.com>
2026-06-03 22:26:55 +0300

    Beware - NULL Values Can Trip You Up When Using Dapper

---
+++ DapperNulls/Program.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+using (var cn = new SqlConnection("data source=;;uid=sa;pwd=YourStrongPassword123;TrustServerCertificate=true"))
+{
+    var date = cn.QuerySingle<DateTime>("SELECT GetDate()");
+    Console.WriteLine(date);
+}
+
+using (var cn = new SqlConnection("data source=;;uid=sa;pwd=YourStrongPassword123;TrustServerCertificate=true"))
+{
+    var date = cn.QuerySingle<DateTime>("SELECT NULL");
+    Console.WriteLine(date);
+}
+
+using (var cn = new SqlConnection("data source=;;uid=sa;pwd=YourStrongPassword123;TrustServerCertificate=true"))
+{
+    var date = cn.QuerySingleOrDefault<DateTime>("SELECT NULL");
+    Console.WriteLine(date);
+}
+
+using (var cn = new SqlConnection("data source=;;uid=sa;pwd=YourStrongPassword123;TrustServerCertificate=true"))
+{
+    var date = cn.QuerySingle<DateTime?>("SELECT NULL");
+    if (date.HasValue)
+        Console.WriteLine(date);
+    else
+        Console.WriteLine("A NULL was returned");
+}
