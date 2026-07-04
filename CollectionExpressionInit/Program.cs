ff4ddf85491f83b7c06ac6084fe0f4e7c010687a
Conrad Akunga <conradakunga@gmail.com>
2026-07-04 14:46:58 +0300

    .NET 11 Preview - Collection Expression Arguments

---
+++ CollectionExpressionInit/Program.cs
@@ -0,0 +1,31 @@
+﻿{
+    // Initialize a list
+    List<string> namesList = new List<string>() { "Brenda", "Latisha", "Linda", "Felicia" };
+    // Initialize an array
+    string[] nameArray = new string[] { "Brenda", "Latisha", "Linda", "Felicia" };
+    // Initialize a HashSet
+    HashSet<string> nameHashSet = new HashSet<string> { "Brenda", "Latisha", "Linda", "Felicia" };
+}
+{
+    // Initialize a list
+    List<string> namesList = new() { "Brenda", "Latisha", "Linda", "Felicia" };
+    // Initialize a HashSet
+    HashSet<string> nameHashSet = new() { "Brenda", "Latisha", "Linda", "Felicia" };
+}
+{
+    // Initialize a list
+    List<string> namesList = ["Brenda", "Latisha", "Linda", "Felicia"];
+    // Initialize an array
+    string[] nameArray = ["Brenda", "Latisha", "Linda", "Felicia"];
+    // Initialize a HashSet
+    HashSet<string> nameHashSet = ["Brenda", "Latisha", "Linda", "Felicia"];
+}
+
+{
+    List<string> namesList = [with(capacity: 4), "Brenda", "Latisha", "Linda", "Felicia"];
+    namesList.ForEach(Console.WriteLine);
+}
+{
+    string[] otherCollection = ["Brenda", "Latisha", "Linda", "Felicia"];
+    List<string> namesList = [with(capacity: 4), .. otherCollection];
+}
