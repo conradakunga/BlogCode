4b9c1f8dc51fce4d937c57ba11baff41d8f5af6e
Conrad Akunga <conradakunga@gmail.com>
2026-07-06 22:35:21 +0300

    .NET 11 Preview - Starting and Forgetting A Process

---
+++ FireAndForget/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+
+//
+// The old way
+//
+
+Process.Start(new ProcessStartInfo
+{
+    FileName = "ping",
+    Arguments = "-v google.com",
+    CreateNoWindow = true
+});
+
+//
+// The new way
+//
+
+Process.StartAndForget("ping", ["-v", "conradakunga.com"]);
