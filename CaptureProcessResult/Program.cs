199001d86dcc71b71d0aa7f888318b81735fafc3
Conrad Akunga <conradakunga@gmail.com>
2026-07-06 22:20:33 +0300

    .NET 11 Preview - Capturing Process Exit Status

---
+++ CaptureProcessResult/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Serilog;
+
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+//
+// The old way
+//
+
+var startInfo = new ProcessStartInfo
+{
+    FileName = "pwd",
+    Arguments = "",
+    RedirectStandardOutput = true,
+    RedirectStandardError = true,
+    UseShellExecute = false,
+    CreateNoWindow = true
+};
+
+using (var process = Process.Start(startInfo))
+{
+    await process.WaitForExitAsync();
+
+    if (process.ExitCode == 0)
+        Log.Information("Success!");
+
+    else
+        Log.Error("Failed!");
+}
+
+//
+// The new way
+//
+
+var result = await Process.RunAsync("pwd", ["-l"]);
+if (result.ExitCode == 0)
+    Log.Information("Success");
+else
+    Log.Error("Failure");
