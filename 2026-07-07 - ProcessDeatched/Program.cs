b5341f95aa095c28107fd6cc9271fdbc21caf80f
Conrad Akunga <conradakunga@gmail.com>
2026-07-08 17:14:51 +0300

    new post - .NET 11 Preview - Starting A Detached Process

---
+++ 2026-07-07 - ProcessDeatched/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+var info = new ProcessStartInfo
+{
+    FileName = "ping",
+    Arguments = "google.com",
+    StartDetached = true,
+    UseShellExecute = false
+};
+
+// Start the detached process
+Process.StartAndForget(info);
