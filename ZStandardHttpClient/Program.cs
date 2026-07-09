b1e3c0e60ca658163adee06cc7de0398a75ac253
Conrad Akunga <conradakunga@gmail.com>
2026-07-09 23:21:11 +0300

    .NET 11 Preview - Using Zstandard Compression With A HttpClient

---
+++ ZStandardHttpClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+// Write a handler to configure compression
+var handler = new HttpClientHandler
+{
+    AutomaticDecompression = DecompressionMethods.Zstandard
+};
+
+// Setup the httpClient
+var client = new HttpClient(handler);
+
+// Fetch the response
+var response = await client.GetStringAsync("https://facebook.com");
+
+Console.WriteLine(response.Length);
