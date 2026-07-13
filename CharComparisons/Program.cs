2784105f3814758af6a199808c3de46bab00d6f6
Conrad Akunga <conradakunga@gmail.com>
2026-07-13 22:44:47 +0300

    .NET 11 Preview - Character Equality Comparison

---
+++ CharComparisons/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine('i'.Equals('I', StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine('i'.Equals('I', StringComparison.CurrentCulture));
+Console.WriteLine('i'.Equals('I', StringComparison.InvariantCulture));
+Console.WriteLine('i'.Equals('I', StringComparison.InvariantCultureIgnoreCase));
+Console.WriteLine('i'.Equals('I', StringComparison.Ordinal));
+Console.WriteLine('i'.Equals('I', StringComparison.OrdinalIgnoreCase));
+
+
+Console.WriteLine('i'.Equals('I'));
+
+Console.WriteLine(string.Equals('i'.ToString(), 'I'.ToString(), StringComparison.CurrentCultureIgnoreCase));
