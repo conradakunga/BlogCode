41fa524bff6e8203e8aaebc0ea08b6ae0fc7c2b7
Conrad Akunga <conradakunga@gmail.com>
2026-07-13 22:02:52 +0300

    .NET 11 Preview - Disabling Zstandard Compression in Kestrel

---
+++ 2026-07-11 - WebServer/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+// Register the response compression services
+builder.Services.AddResponseCompression(options =>
+{
+    //
+    // Remove all the providers
+    //
+    options.Providers.Clear();
+
+    //
+    // Add back the ones we want
+    //
+
+    // First, brotli
+    options.Providers.Add<BrotliCompressionProvider>();
+    // Next, gzip
+    options.Providers.Add<GzipCompressionProvider>();
+});
+var app = builder.Build();
+// Turn on the middleware
+app.UseResponseCompression();
+// Register a route
+app.MapGet("/Hello", () => "The quick brown fox jumped over the lazy dog");
+// Start the application
+app.Run();
