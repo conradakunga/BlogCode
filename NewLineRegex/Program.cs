d0de93b0b0d8463fd5ea11c35840fa302da88be5
Conrad Akunga <conradakunga@gmail.com>
2026-07-15 10:37:44 +0300

    .NET 11 Preview - RegularExpression NewLine Handling Improvement

---
+++ NewLineRegex/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+var text = "The Quick\r\nBrown Fox\u0085Jumped Over\u2028The Lazy Dog\nBigly";
+
+var oldLines = Regex.Matches(text, @"^.*$", RegexOptions.Multiline).Select(r => r.Value).ToArray();
+
+foreach (var line in oldLines)
+{
+    Console.WriteLine(line);
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+var newLines = Regex.Matches(text, @"^.*$", RegexOptions.Multiline | RegexOptions.AnyNewLine).Select(r => r.Value)
+    .ToArray();
+
+foreach (var line in newLines)
+{
+    Console.WriteLine(line);
+}
