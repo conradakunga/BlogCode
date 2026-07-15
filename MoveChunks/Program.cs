e5bed2dcbd51d73580b928d332dc4cd52e6ee7ee
Conrad Akunga <conradakunga@gmail.com>
2026-07-15 09:59:12 +0300

    .NET 11 Preview - Moving StringBuilder Content With MoveChunks

---
+++ MoveChunks/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+// Generate a long string the hard way	
+var str = "";
+for (var i = 1; i < 10_000; i++)
+{
+    str += $"{i} ";
+}
+
+Console.WriteLine(str);
+
+// Generate using a StringBuilder
+StringBuilder sb = new();
+for (var i = 1; i < 10_000; i++)
+{
+    sb.Append($"{i} ");
+}
+
+Console.WriteLine(sb.ToString());
+
+// Copy from one StringBuilder to another
+StringBuilder oldTarget = new StringBuilder(sb.ToString());
+
+// Move from one StringBuilder to another
+Console.WriteLine($"The source has {sb.Length} characters before");
+StringBuilder target = StringBuilder.MoveChunks(sb);
+Console.WriteLine($"The source has {sb.Length} characters after");
