ead93ea40c0e2f3af758ae2e59ce28c97214ff84
Conrad Akunga <conradakunga@gmail.com>
2026-07-15 21:26:28 +0300

    .NET 11 Preview - BitArray Improvements - PopCount

---
+++ BitArrayPop/Program.cs
@@ -0,0 +1,24 @@
+﻿// Create our strucutre
+
+using System.Collections;
+
+var arr = new BitArray(10_000)
+{
+    // Set some of the bits
+    [1000] = true,
+    [100] = true
+};
+
+// Check if any seat is occuped
+Console.WriteLine($"Is at least one seat occupied? {arr.HasAnySet()}");
+
+int count = 0;
+foreach (bool element in arr)
+{
+    if (element)
+        count++;
+}
+
+Console.WriteLine($"There are {count} seats occupied");
+
+Console.WriteLine($"There are {arr.PopCount()} seats occupied");
