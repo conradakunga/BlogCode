cf46e1c396c1ac43b8e3d2ff6c69054e5a540067
Conrad Akunga <conradakunga@gmail.com>
2026-07-18 20:16:20 +0300

    .NET 11 Preview - Finding A Running Process

---
+++ RunningProcesses/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+
+{
+    try
+    {
+        var process = Process.GetProcessById(99999);
+
+        Console.WriteLine($"Process name: {process.ProcessName}");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Could not load process!");
+    }
+}
+
+{
+    if (Process.TryGetProcessById(99999, out var process))
+    {
+        // Do stuff with our process here
+        Console.WriteLine($"Process name: {process.ProcessName}");
+    }
+    else
+    {
+        Console.WriteLine("Could not load process!");
+    }
+}
