ba0a6e48c2a64f74e7bb9fcb324965d526a8e0fa
Conrad Akunga <conradakunga@gmail.com>
2026-07-18 20:09:45 +0300

    .NET 11 Preview - Stream Adapters

---
+++ StreamAdapters/Program.cs
@@ -0,0 +1,20 @@
+﻿// Get our data
+
+using System.Text;
+
+var data =
+    """
+    The quick brown fox
+    mightily jumped over
+    the brown dog very
+    very bigly
+    """;
+
+// Convert our data a byte array
+var dataInBytes = Encoding.Default.GetBytes(data);
+
+// Convert our byte array into a memory stream
+var dataStream = new MemoryStream(dataInBytes);
+
+// Directly create a stream
+var directDataStream = new StringStream(data, Encoding.UTF8);
