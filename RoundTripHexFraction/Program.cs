34543f321265e76487ca046f5b6769873d9530f4
Conrad Akunga <conradakunga@gmail.com>
2026-07-27 02:07:56 +0300

    .NET 11 Preview - Round-tripping Hex Formatted Fractional Numbers

---
+++ RoundTripHexFraction/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+var natruralLog = Math.E;
+
+var hexValue = natruralLog.ToString("x");
+
+var number = double.Parse(hexValue, NumberStyles.HexFloat);
+
+// Print number
+Console.WriteLine(number);
+
+// Verify parity
+Console.WriteLine(number == natruralLog);
