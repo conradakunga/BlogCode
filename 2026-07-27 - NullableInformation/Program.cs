b6faccefbc7c2c2dac1a0d1091c3aa862451ff8e
Conrad Akunga <conradakunga@gmail.com>
2026-07-27 22:38:47 +0300

    Minor refactoring

--- 2026-07-27 - NullableInformation/Program.cs
+++ 2026-07-27 - NullableInformation/Program.cs
@@ -4,10 +4,11 @@
     Type nullableIntType = typeof(int?);
     Console.WriteLine(nullableIntType.Name);
     Type? underlying = Nullable.GetUnderlyingType(nullableIntType);
-    Console.WriteLine(underlying.Name); // 
+    Console.WriteLine(underlying.Name);
 }
 
 // .NET 11
+
 {
     Type nullableIntType = typeof(int?);
     Type? underlying = nullableIntType.GetNullableUnderlyingType();
