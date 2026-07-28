fc8e8950bc3b395a272a98413533d41c7ac2c2fe
Conrad Akunga <conradakunga@gmail.com>
2026-07-28 22:14:11 +0300

    .NET 11 Preview - X25519 Diffie-Hellman

---
+++ CryptoECDiffieHellman/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Cryptography;
+
+{
+    using (var alice = ECDiffieHellman.Create(ECCurve.NamedCurves.nistP256))
+    {
+        using (var bob = ECDiffieHellman.Create(ECCurve.NamedCurves.nistP256))
+        {
+            // Get alice's secret in a byte array
+            var aliceSecret = alice.DeriveKeyMaterial(bob.PublicKey);
+            // Get bob's secret in a byte array
+            var bobSecret = bob.DeriveKeyMaterial(alice.PublicKey);
+
+            // verify the secrets match
+            var match = CryptographicOperations.FixedTimeEquals(aliceSecret, bobSecret);
+            Console.WriteLine(match);
+        }
+    }
+}
+// .NET 11
+{
+    // Generate key pairs for alice
+    using (var alice = X25519DiffieHellman.GenerateKey())
+    {
+        // Generate key pairs for bob
+        using (var bob = X25519DiffieHellman.GenerateKey())
+        {
+            // get alice's secret with bob's key
+            byte[] aliceShared = alice.DeriveRawSecretAgreement(bob);
+            // get boob's secret with alice's key
+            byte[] bobShared = bob.DeriveRawSecretAgreement(alice);
+            // Check if they are equal
+            Console.WriteLine(CryptographicOperations.FixedTimeEquals(aliceShared, bobShared));
+        }
+    }
+}
+{
+    // Generate key pairs for alice
+    using var alice = X25519DiffieHellman.GenerateKey();
+    // Generate key pairs for bob
+    using var bob = X25519DiffieHellman.GenerateKey();
+    // get alice's secret with bob's key
+    byte[] aliceShared = alice.DeriveRawSecretAgreement(bob);
+    // get boob's secret with alice's key
+    byte[] bobShared = bob.DeriveRawSecretAgreement(alice);
+    // Check if they are equal
+    Console.WriteLine(CryptographicOperations.FixedTimeEquals(aliceShared, bobShared));
+}
