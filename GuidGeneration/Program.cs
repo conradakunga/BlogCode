47fa24a1d17b6118100492c5756921eda8b96b2d
Conrad Akunga <conradakunga@gmail.com>
2026-08-10 12:36:17 +0300

    Beware - GuidV7 Generation In High Throughput Environments Sorting Gotcha

---
+++ GuidGeneration/Program.cs
@@ -0,0 +1,35 @@
+﻿// With pause
+
+{
+    var list = new List<Guid>();
+
+    for (var i = 0; i < 10; i++)
+    {
+        var temp = Guid.CreateVersion7();
+        Console.WriteLine(temp);
+        list.Add(temp);
+        Thread.Sleep(1);
+    }
+
+    Console.WriteLine();
+
+    list.Sort();
+
+    list.ForEach(x => Console.WriteLine(x));
+}
+// Without pause
+{
+    var list = new List<Guid>();
+    for (var i = 0; i < 10; i++)
+    {
+        var temp = Guid.CreateVersion7();
+        Console.WriteLine(temp);
+        list.Add(temp);
+    }
+
+    Console.WriteLine();
+
+    list.Sort();
+
+    list.ForEach(x => Console.WriteLine(x));
+}
