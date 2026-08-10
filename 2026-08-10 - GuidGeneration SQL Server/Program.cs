8695ed3c0f900993d671adce84489a8b77b5dad3
Conrad Akunga <conradakunga@gmail.com>
2026-08-10 18:27:01 +0300

    GuidV7 Considerations for SQL Server

--- 2026-08-10 - GuidGeneration SQL Server/Program.cs
+++ 2026-08-10 - GuidGeneration SQL Server/Program.cs
@@ -1,20 +1,18 @@
 ﻿using Dapper;
-using Microsoft.Data.SqlClient;
+using Npgsql;
 
-const string connectionString =
-    "data source=localhost;uid=sa;password=YourStrongPassword123;database=things;trustservercertificate=true";
+const string connectionString = "host=localhost;username=myuser;password=mypassword;database=things";
 var things = new List<Thing>();
 for (var i = 0; i < 10; i++)
 {
     things.Add(new Thing(Guid.CreateVersion7(), $"{i}"));
-    Thread.Sleep(TimeSpan.FromMilliseconds(1));
 }
 
 foreach (var thing in things)
 {
     Console.WriteLine($"ID: {thing.ID}; {thing.Caption}");
 
-    using (var cn = new SqlConnection(connectionString))
+    using (var cn = new NpgsqlConnection(connectionString))
     {
         cn.Execute("insert into things(id,caption) values (@ID,@Caption)", thing);
     }
