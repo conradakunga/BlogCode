4d8ea52e1dad0013020e93552f164644e93586f9
Conrad Akunga <conradakunga@gmail.com>
2026-08-10 17:46:04 +0300

    GuidV7 Considerations for Database Keys

---
+++ 2026-08-08 - GuidGeneration Database/Program.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using Npgsql;
+
+const string connectionString = "host=localhost;username=myuser;password=mypassword;database=things";
+var things = new List<Thing>();
+for (var i = 0; i < 10; i++)
+{
+    things.Add(new Thing(Guid.CreateVersion7(), $"{i}"));
+}
+
+foreach (var thing in things)
+{
+    Console.WriteLine($"ID: {thing.ID}; {thing.Caption}");
+
+    using (var cn = new NpgsqlConnection(connectionString))
+    {
+        cn.Execute("insert into things(id,caption) values (@ID,@Caption)", thing);
+    }
+}
