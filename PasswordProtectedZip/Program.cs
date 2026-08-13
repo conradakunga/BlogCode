c0635996b0b6117502c8577e3e4bc4b821bea0d3
Conrad Akunga <conradakunga@gmail.com>
2026-08-13 21:16:03 +0300

    .NET 11 Preview - Generating Password-Protected Zip Archives

---
+++ PasswordProtectedZip/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO.Compression;
+using System.Reflection;
+using Serilog;
+
+// Setup logging
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+// Set up the location of the target zip file
+string targetZipFile =
+    Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "WarAndPeace.zip");
+// Set up the zip file password
+const string ZipFilePassword = "LeoTolstoy123%#";
+
+// Create the archive
+await using (var archive = ZipFile.Open(targetZipFile, ZipArchiveMode.Create))
+{
+    // Add the file to the zip archive
+    await archive.CreateEntryFromFileAsync("war-and-peace.txt", "War And Peace",
+        CompressionLevel.SmallestSize,
+        ZipFilePassword.AsMemory(), ZipEncryptionMethod.Aes256, CancellationToken.None);
+
+    Log.Information("Compressed file to {ZipFile}", new FileInfo(targetZipFile).Name);
+}
