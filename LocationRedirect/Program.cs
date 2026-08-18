8286835e22e1ed7c3117468e3ca859418c2e3e70
Conrad Akunga <conradakunga@gmail.com>
2026-08-18 19:44:52 +0300

    .NET 11 Preview - Overriding Assembly Location

---
+++ LocationRedirect/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.Loader;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+Log.Information("Default root location is {Original}", Assembly.GetExecutingAssembly().Location);
+
+const string customDirectory = @"/User/rad/Documents/plugins/";
+
+AssemblyLoadContext.SetAssemblyLocationOverride((assembly, defaultLocation) =>
+{
+    // View the default values
+    Log.Warning("Original resolved location is {Original}", defaultLocation);
+
+    // Get the assembly name
+    var assemblyName = assembly.GetName().Name;
+
+    if (assemblyName is not null)
+        return Path.Combine(customDirectory, assemblyName + ".dll");
+    return defaultLocation;
+});
+
+Log.Information("New resolved location is {Original}", Assembly.GetExecutingAssembly().Location);
+
+// Change the location
+AssemblyLoadContext.SetAssemblyLocationOverride((assembly, defaultLocation) =>
+{
+    // View the default values
+    Log.Warning("Original resolved location is {Original}", defaultLocation);
+
+    // Get the assembly name
+    var assemblyName = assembly.GetName().Name;
+
+    if (assemblyName is not null)
+        return Path.Combine(customDirectory, assemblyName + "2.dll");
+    return defaultLocation;
+});
