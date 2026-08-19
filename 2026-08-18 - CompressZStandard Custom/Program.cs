a6f3a78fb3eb2fa8338a1d27cdd97e3c7a346882
Conrad Akunga <conradakunga@gmail.com>
2026-08-19 19:27:36 +0300

    .NET 11 Preview - Customizing HttpClient Zstandard Compression

---
+++ 2026-08-18 - CompressZStandard Custom/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO.Compression;
+using System.Net;
+
+const string API_URL = "https://reqbin.com/echo/post/json";
+
+var jamesBond = new Spy
+{
+    Firstname = "James",
+    Surname = "Bond",
+    Agency = "MI-6",
+    DateOfBirth = new DateOnly(1950, 1, 1),
+    HireDate = new DateOnly(1975, 1, 1)
+};
+
+var handler = new HttpClientHandler
+{
+    AutomaticDecompression = DecompressionMethods.Zstandard
+};
+
+var client = new HttpClient(handler);
+await client.PostAsJsonAsync(API_URL, jamesBond);
+
+// Create the payload
+var payload = JsonContent.Create(jamesBond);
+// Create a HttpRequest
+using var request = new HttpRequestMessage(HttpMethod.Post, API_URL);
+// Set-up compression options
+var options = new ZstandardCompressionOptions
+{
+    Quality = 6,
+    AppendChecksum = true,
+    EnableLongDistanceMatching = false
+};
+// Compress the content
+request.Content = new ZstandardCompressedContent(payload, options);
+//Post
+await client.SendAsync(request);
---
+++ 2026-08-18 - CompressZStandard Custom/Spy.cs
@@ -0,0 +1,8 @@
+public sealed class Spy
+{
+    public required string Firstname { get; set; }
+    public required string Surname { get; set; }
+    public required string Agency { get; set; }
+    public required DateOnly DateOfBirth { get; set; }
+    public required DateOnly HireDate { get; set; }
+}
