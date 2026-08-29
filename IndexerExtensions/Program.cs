27d56c2d168cadbf1a094b7de06e0feee67c0e90
Conrad Akunga <conradakunga@gmail.com>
2026-08-29 03:08:43 +0300

    .NET 11 Preview - Extension Indexers

---
+++ IndexerExtensions/Program.cs
@@ -0,0 +1,19 @@
+﻿var str = "cocoa butter".AsEnumerable();
+Console.WriteLine((char)str[0]);
+
+
+// public static class EnumerableExtensions
+// {
+// 	public static char ItemAt(this IEnumerable<char> str, int index)
+// 	{
+// 		return str.ElementAt(index);
+// 	}
+// }
+
+public static class EnumerableExtensions
+{
+    extension(IEnumerable<char> enumerable)
+    {
+        public int this[int index] => enumerable.ElementAt(index);
+    }
+}
