4653b37c28fbeda29c56ad2acabf2ae894bfe476
Conrad Akunga <conradakunga@gmail.com>
2026-09-12 22:14:33 +0300

    Boxing and Unboxing Surprise in Java

---
+++ 2026-08-27  - BoxingDemo C#/Program.cs
@@ -0,0 +1,9 @@
+﻿Int32 first = 100;
+Int32 second = 100;
+
+Console.WriteLine(first == second);
+
+first = 200;
+second = 200;
+
+Console.WriteLine(first == second);
---
+++ 2026-08-27 - BoxingDemo Java/src/Main.java
@@ -0,0 +1,22 @@
+void main() {
+//    Integer first = 100;
+//    Integer second = 100;
+//
+//    IO.println(first == second);
+//
+//    first = 200;
+//    second = 200;
+//
+//    IO.println(first == second);
+
+
+    int first = 100;
+    int second = 100;
+
+    IO.println(first == second);
+
+    first = 200;
+    second = 200;
+
+    IO.println(first == second);
+}
