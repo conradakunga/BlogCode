fa6fbc9bbe21cfd323044214014c69ce33ce8e11
Conrad Akunga <conradakunga@gmail.com>
2026-09-14 19:49:43 +0300

    .NET 11 Preview - Native DNS Resolving

---
+++ DNSResolution/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+var result = await Dns.ResolveSrvAsync("_imaps._tcp.gmail.com");
+
+if (result.ResponseCode == DnsResponseCode.NoError)
+{
+    if (result.Records.Count == 0)
+    {
+        Console.WriteLine("No SRV records found.");
+    }
+    else
+    {
+        foreach (var record in result.Records)
+        {
+            Console.WriteLine($"{record.Target}:{record.Port} Priority={record.Priority} Weight={record.Weight}");
+        }
+    }
+}
