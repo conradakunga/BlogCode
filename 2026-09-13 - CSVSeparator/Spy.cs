e287409464db9cb61a612d2b9b99e99529835a71
Conrad Akunga <conradakunga@gmail.com>
2026-09-25 18:57:00 +0300

    Leading Spaces Considerations When Using CSVHelper

---
+++ 2026-09-13 - CSVSeparator/Spy.cs
@@ -0,0 +1,6 @@
+public sealed class Spy
+{
+    public required string Firstname { get; init; }
+    public required string Lastname { get; init; }
+    public required DateOnly DateOfBirth { get; init; }
+}
