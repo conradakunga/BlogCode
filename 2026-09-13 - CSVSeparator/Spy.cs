3891799297394d1d0a6dd504d3a7d44aa8524a9a
Conrad Akunga <conradakunga@gmail.com>
2026-09-26 04:20:41 +0300

    Rename folders

--- 2026-09-13 - CSVSeparator/Spy.cs
+++
@@ -1,6 +0,0 @@
-public sealed class Spy
-{
-    public required string Firstname { get; init; }
-    public required string Lastname { get; init; }
-    public required DateOnly DateOfBirth { get; init; }
-}
